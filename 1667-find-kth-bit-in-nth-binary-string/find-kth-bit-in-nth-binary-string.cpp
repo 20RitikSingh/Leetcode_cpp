73659f03030822008ad2c5c8ff57489e1073fef3
Ritik Singh <92461786+20RitikSingh@users.noreply.github.com>
2024-10-20 02:04:34 +0530

    Time: 0 ms (100.00%) | Memory: 7.4 MB (64.26%) - LeetSync

--- 1667-find-kth-bit-in-nth-binary-string/find-kth-bit-in-nth-binary-string.cpp
+++ 1667-find-kth-bit-in-nth-binary-string/find-kth-bit-in-nth-binary-string.cpp
@@ -2,19 +2,9 @@ class Solution {
 public:
     char findKthBit(int n, int k) {
         if(n==1) return '0';
-        int t=pow(2,n)-1;
-        if(k==(t+1)/2) return '1';
-        if(k>(t+1)/2){
-            k-=(t+1)/2;
-            char ch=findKthBit(n-1,pow(2,n-1)-k);
-            // cout<<n-1<<" "<<pow(2,n-1)-k<<" not:"<<ch<<endl;
-            if(ch=='0') return '1';
-            return '0'; 
-        }
-        char ch=findKthBit(n-1,k);
-        // cout<<n-1<<" "<<k<<" "<<ch<<endl;
-        return ch;
+        int t=pow(2,n-1);
+        if(k==t) return '1';
+        if(k>t) return findKthBit(n-1,2*t-k)=='0'?'1':'0';
+        return findKthBit(n-1,k);
     }
 };
-// 1 3 7 15 31 63
-//  2 4 8 16 32
