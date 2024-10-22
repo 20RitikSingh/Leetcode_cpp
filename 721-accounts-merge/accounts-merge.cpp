f76277e24689864583edc829d5f2f75d6114db9b
Ritik Singh <92461786+20RitikSingh@users.noreply.github.com>
2024-10-22 05:22:14 -0700

    Time: 68 ms (61.88%) | Memory: 37.3 MB (84.48%) - LeetSync

--- 721-accounts-merge/accounts-merge.cpp
+++ 721-accounts-merge/accounts-merge.cpp
@@ -2,41 +2,34 @@ class Solution {
 public:
     vector<vector<string>> accountsMerge(vector<vector<string>>& acc) {
         int n=acc.size();
-        vector<int> parent(n);
-        for(int i=0;i<n;i++) parent[i]=i;
         unordered_map<string,int> mp;
         vector<unordered_set<string>> mem(n);
+        vector<vector<string>> res;
+        vector<int> parent(n);
+                
         for(int i=0;i<n;i++){
+            parent[i]=i;
             for(int j=1;j<acc[i].size();j++){
                 if(mp.find(acc[i][j])!=mp.end()){ 
-                    int x=mp[acc[i][j]];
-                    while(x!=parent[x]) x=parent[x];
-                    if(parent[i]!=x){
-                        parent[parent[i]]=x;
-                    }
-                    parent[i]=x; 
-                }
-                mp[acc[i][j]]=i;
+                    while(mp[acc[i][j]]!=parent[mp[acc[i][j]]]) mp[acc[i][j]]=parent[mp[acc[i][j]]];
+                    if(parent[i]!=mp[acc[i][j]]) parent[parent[i]]=mp[acc[i][j]];
+                    parent[i]=mp[acc[i][j]]; 
+                }else mp[acc[i][j]]=i;
             }
         }
+
         for(int i=0;i<n;i++){
-            int x=parent[i];
-            while(x!=parent[x]) x=parent[x];
-            parent[i]=x;
-        }
-        // for(int i=0;i<n;i++) cout<<i<<"->"<<parent[i]<<endl;
-        for(int i=0;i<n;i++){
+            while(parent[i]!=parent[parent[i]]) parent[i]=parent[parent[i]];
             for(int j=1;j<acc[i].size();j++) mem[parent[i]].insert(acc[i][j]);
         }
-        vector<vector<string>> res;
+        
         for(int i=0;i<n;i++){
-           if(mem[i].empty()) continue;
-           res.push_back({acc[i][0]});
-           for(auto s:mem[i]) res.back().push_back(s); 
+            if(mem[i].empty()) continue;
+            res.push_back({acc[i][0]});
+            for(auto s:mem[i]) res.back().push_back(s); 
         }
-        for(auto &v:res){
-            sort(v.begin()+1,v.end());
-        }
+        
+        for(auto &v:res) sort(v.begin()+1,v.end());
         return res;
     }
 };
