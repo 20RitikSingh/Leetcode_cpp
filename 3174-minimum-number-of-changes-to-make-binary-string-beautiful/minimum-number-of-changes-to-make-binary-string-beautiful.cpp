58dd9b20a7a31f4e2626d2d8c24625207d0459e3
Ritik Singh <92461786+20RitikSingh@users.noreply.github.com>
2024-11-06 10:31:41 -0800

    Time: 1 ms (54.05%) | Memory: 12.1 MB (67.88%) - LeetSync

--- 3174-minimum-number-of-changes-to-make-binary-string-beautiful/minimum-number-of-changes-to-make-binary-string-beautiful.cpp
+++ 3174-minimum-number-of-changes-to-make-binary-string-beautiful/minimum-number-of-changes-to-make-binary-string-beautiful.cpp
@@ -1,21 +1,8 @@
 class Solution {
 public:
     int minChanges(string s) {
-        int prev=s[0],c=0,k=0,f=0;
-        for(int i:s){
-            if(prev!=i){
-                if(c%2){
-                    f=!f;
-                    k+=f;
-                }else if(f) k++;
-                c=1;
-                prev=i;
-            }else c++;
-        }
-        if(c%2){
-            f=!f;
-            k+=f;
-        }else if(f) k++;
+        int k=0;
+        for(int i=0;i<s.size();i+=2) k+=(s[i]!=s[i+1]);
         return k;
     }
 };
