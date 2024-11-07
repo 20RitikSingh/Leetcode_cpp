8f8c56875ce1f1970b3f2ed3d1c8109f79843e6e
Ritik Singh <92461786+20RitikSingh@users.noreply.github.com>
2024-11-07 06:35:13 -0800

    Time: 11 ms (74.92%) | Memory: 15.2 MB (36.16%) - LeetSync

---
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        set<int> set;
+        for(int i:nums){
+            auto it=set.lower_bound(i);
+            if(it!=set.end()){
+                set.erase(it);
+            }
+            set.insert(i);
+        }
+        return set.size();
+    }
+};
