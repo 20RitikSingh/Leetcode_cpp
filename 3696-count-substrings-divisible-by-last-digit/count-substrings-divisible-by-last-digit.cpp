ab865a65b38196da7d50ce67f356a4ab7bc95959
Ritik Singh <92461786+20RitikSingh@users.noreply.github.com>
2025-02-10 15:14:43 -0800

    Time: 1733 ms (28.46%) | Memory: 19.2 MB (74.55%) - LeetSync

--- 3696-count-substrings-divisible-by-last-digit/count-substrings-divisible-by-last-digit.cpp
+++ 3696-count-substrings-divisible-by-last-digit/count-substrings-divisible-by-last-digit.cpp
@@ -9,25 +9,19 @@ public:
             for(int d=1;d<10;d++){
                 for(int r=0;r<10;r++){
 
-                    if(idx==0){ 
-                        dp[1][d][r]= (s[0]%d)==r;
-                        continue;
-                    }
-
                     dp[1][d][r]=(s[idx]%d)==r;
 
                     if((10%d)==0){
-                        if(r!=(s[idx]%d)){
-                            dp[1][d][r]=0;
-                            continue;
-                        }
+
+                        if(r!=(s[idx]%d)) continue;
+
                         for(int i=0;i<d;i++){
                             dp[1][d][r]+=dp[0][d][i];
                         }
-                    }else{
-                        for(int i=0;(i*d+r-(s[idx]%d))/(10%d)<d;i++){
-                            if((i*d+r-(s[idx]%d))>=0 && (i*d+r-(s[idx]%d))%(10%d)==0) dp[1][d][r]+=dp[0][d][(i*d+r-(s[idx]%d))/(10%d)];
-                        }
+                        continue;
+                    }
+                    for(int i=0;(i*d+r-(s[idx]%d))/(10%d)<d;i++){
+                        if((i*d+r-(s[idx]%d))>=0 && (i*d+r-(s[idx]%d))%(10%d)==0) dp[1][d][r]+=dp[0][d][(i*d+r-(s[idx]%d))/(10%d)];
                     }
                 }
             }
