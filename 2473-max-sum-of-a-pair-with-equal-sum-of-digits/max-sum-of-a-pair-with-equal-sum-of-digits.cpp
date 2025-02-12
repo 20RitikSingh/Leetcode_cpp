3feaca7a8b8afd931adbf48ca4970bcaf5a4d9d3
Ritik Singh <92461786+20RitikSingh@users.noreply.github.com>
2025-02-12 11:54:36 -0800

    Time: 12 ms (88.15%) | Memory: 63.8 MB (75.53%) - LeetSync

--- 2473-max-sum-of-a-pair-with-equal-sum-of-digits/max-sum-of-a-pair-with-equal-sum-of-digits.cpp
+++ 2473-max-sum-of-a-pair-with-equal-sum-of-digits/max-sum-of-a-pair-with-equal-sum-of-digits.cpp
@@ -8,12 +8,15 @@ public:
                 x+=i%10;
                 i/=10;
             }
-            v[x].push_back(j);
+            if(v[x].size()<2) v[x].push_back(j);
+            else{
+                if(v[x][0]<v[x][1]) swap(v[x][0],v[x][1]);
+                v[x][1]=max(j,v[x][1]);
+            }
         }
         int mx=-1;
         for(int i=0;i<82;i++){
             if(v[i].size()<2) continue;
-            sort(v[i].begin(),v[i].end(),greater<int>());
             mx=max(mx,v[i][0]+v[i][1]);
         }
         return mx;
