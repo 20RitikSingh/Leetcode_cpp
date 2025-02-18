dc75b6b4e8f3c20b4d5e5c0a4cfd7f4aac278073
Ritik Singh <92461786+20RitikSingh@users.noreply.github.com>
2025-02-18 01:06:27 -0800

    Time: 0 ms (100.00%) | Memory: 7.8 MB (73.43%) - LeetSync

--- 2456-construct-smallest-number-from-di-string/construct-smallest-number-from-di-string.cpp
+++ 2456-construct-smallest-number-from-di-string/construct-smallest-number-from-di-string.cpp
@@ -1,33 +1,41 @@
 class Solution {
-    void perm(string s,vector<string> &v,int idx=0){
-        if(idx>=s.size()){
-            // cout<<s<<endl;
-            v.push_back(s);
-            return;
-        }
-        for(int i=idx;i<s.size();i++){
-            swap(s[i],s[idx]);
-            perm(s,v,idx+1);
-            swap(s[i],s[idx]);
-        }
-        return;
-    }
-    bool isvalid(string s,string pattern){
-        for(int i=0;i<pattern.size();i++){
-            if(pattern[i]=='I' && s[i]>s[i+1]) return 0;
-            if(pattern[i]=='D' && s[i]<s[i+1]) return 0;
-        }
-        return 1;
-    }
 public:
-    string smallestNumber(string pattern) {
-        string s="123456789";
-        s=s.substr(0,pattern.size()+1);
-        vector<string> v;
-        perm(s,v);
-        for(auto str:v){
-            if(isvalid(str,pattern)) return str;
+    string smallestNumber(string p) {
+        string s;
+        int a=1,d=0;
+        for(int i=0;i<p.size();i++){
+            if(p[i]=='D'){
+                d++;
+                continue;
+            }
+            if(d){
+                string t;
+                for(int i=0;i<=d;i++){
+                    t.push_back('0'+a);
+                    a++;
+                }
+                reverse(t.begin(),t.end());
+                s+=t;
+                d=0;
+                continue;
+            }
+            if(p[i]=='I'){
+                s.push_back('0'+a);
+                a++;
+            }
         }
-        return v.back();
+        if(d){
+            string t;
+            for(int i=0;i<=d;i++){
+                if(a>9) continue;
+                t.push_back('0'+a);
+                a++;
+            }
+            reverse(t.begin(),t.end());
+            s+=t;
+            d=0;
+        }
+        if(s.size()==p.size()) s.push_back('0'+a);
+        return s;
     }
 };
