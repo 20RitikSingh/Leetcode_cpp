b5e763420ce7fca6086b24f521f54c5789e107c1
Ritik Singh <92461786+20RitikSingh@users.noreply.github.com>
2025-03-07 04:54:49 -0800

    Time: 3 ms (92.22%) | Memory: 10.4 MB (82.22%) - LeetSync

--- 2610-closest-prime-numbers-in-range/closest-prime-numbers-in-range.cpp
+++ 2610-closest-prime-numbers-in-range/closest-prime-numbers-in-range.cpp
@@ -19,9 +19,7 @@ public:
         auto it1=lower_bound(prime.begin(),prime.end(),left);
         auto it2=upper_bound(prime.begin(),prime.end(),right)-1;
         if(it2-it1<1) return {-1,-1};
-        for(auto it=it1;it<it2;it++){
-            if((it+1)<prime.end() && *(it1+1)-*it1>*(it+1)-*it) it1=it;
-        }
+        for(auto it=it1;it<it2;it++) if(*(it1+1)-*it1>*(it+1)-*it) it1=it;
         return {*it1,*(it1+1)};
     }
 };
