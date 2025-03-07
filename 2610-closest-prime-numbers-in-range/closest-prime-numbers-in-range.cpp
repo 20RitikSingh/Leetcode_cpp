8fd9e408cc310b0d9ce920f4dd1bf9f8399b1c07
Ritik Singh <92461786+20RitikSingh@users.noreply.github.com>
2025-03-07 04:23:19 -0800

    Time: 1454 ms (5.18%) | Memory: 18.4 MB (50.37%) - LeetSync

--- 2610-closest-prime-numbers-in-range/closest-prime-numbers-in-range.cpp
+++ 2610-closest-prime-numbers-in-range/closest-prime-numbers-in-range.cpp
@@ -1,19 +1,19 @@
 #define MAXNUM 1000001
-class Solution {
-    vector<int> composite;
-public:
-    Solution(){
-        composite=vector<int>(MAXNUM);
-        composite[0]=1;
-        composite[1]=1;
-        for(int i=2;i<MAXNUM;i++){
-            if(composite[i]) continue;
-            for(int j=i;1ll*i*j<MAXNUM;j++){
-                composite[i*j]=1;
-            }
+bool composite[MAXNUM];
+void init(){
+    composite[0]=1;
+    composite[1]=1;
+    for(int i=2;i<MAXNUM;i++){
+        if(composite[i]) continue;
+        for(int j=i;1ll*i*j<MAXNUM;j++){
+            composite[i*j]=1;
         }
     }
+}
+class Solution {
+public:
     vector<int> closestPrimes(int left, int right) {
+        if(!composite[0]) init();
         vector<int> ans,res;
         int k=0;
         for(int i=left;i<=right;i++){
@@ -21,8 +21,8 @@ public:
             if(ans.size()>1 && ans[k+1]-ans[k]>i-ans.back()) k=ans.size()-1;
             ans.push_back(i);
         }
-        // cout<<composite[left]<<endl;
-        // for(int i:ans) cout<<i<<" ";
+        cout<<composite[left]<<endl;
+        for(int i:ans) cout<<i<<" ";
         if(ans.size()<2)
             return {-1,-1};
         return {ans[k],ans[k+1]};
