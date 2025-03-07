ce8b33fc95dbc58f83a14b952846c398a30aed55
Ritik Singh <92461786+20RitikSingh@users.noreply.github.com>
2025-03-07 05:08:12 -0800

    Time: 4 ms (90.74%) | Memory: 10.4 MB (82.22%) - LeetSync

--- 2610-closest-prime-numbers-in-range/closest-prime-numbers-in-range.cpp
+++ 2610-closest-prime-numbers-in-range/closest-prime-numbers-in-range.cpp
@@ -1,24 +1,21 @@
 #define MAXNUM 1000001
 bool composite[MAXNUM];
 vector<int> prime;
-void init(){
-    
-}
 class Solution {
 public:
-    Solution(){static bool seive=[](){
-        composite[0]=1;
-        composite[1]=1;
-        for(int i=2;i<MAXNUM;i++){
-            if(composite[i]) continue;
-            prime.push_back(i);
-            for(int j=i;1ll*i*j<MAXNUM;j++){
-                composite[i*j]=1;
-            }
-        }
-        return 1;
-    }();}
     vector<int> closestPrimes(int left, int right) {
+        static bool seive=[](){
+            composite[0]=1;
+            composite[1]=1;
+            for(int i=2;i<MAXNUM;i++){
+                if(composite[i]) continue;
+                prime.push_back(i);
+                for(int j=i;1ll*i*j<MAXNUM;j++){
+                    composite[i*j]=1;
+                }
+            }
+            return 1;
+        }();
         auto it1=lower_bound(prime.begin(),prime.end(),left);
         auto it2=upper_bound(prime.begin(),prime.end(),right)-1;
         if(it2-it1<1) return {-1,-1};
