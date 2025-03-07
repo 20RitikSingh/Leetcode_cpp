41a41223679955d8f46c5a41db2305c2a2357cff
Ritik Singh <92461786+20RitikSingh@users.noreply.github.com>
2025-03-07 05:06:14 -0800

    Time: 6 ms (90.00%) | Memory: 10.4 MB (82.22%) - LeetSync

--- 2610-closest-prime-numbers-in-range/closest-prime-numbers-in-range.cpp
+++ 2610-closest-prime-numbers-in-range/closest-prime-numbers-in-range.cpp
@@ -2,20 +2,23 @@
 bool composite[MAXNUM];
 vector<int> prime;
 void init(){
-    composite[0]=1;
-    composite[1]=1;
-    for(int i=2;i<MAXNUM;i++){
-        if(composite[i]) continue;
-        prime.push_back(i);
-        for(int j=i;1ll*i*j<MAXNUM;j++){
-            composite[i*j]=1;
+    static bool seive=[](){
+        composite[0]=1;
+        composite[1]=1;
+        for(int i=2;i<MAXNUM;i++){
+            if(composite[i]) continue;
+            prime.push_back(i);
+            for(int j=i;1ll*i*j<MAXNUM;j++){
+                composite[i*j]=1;
+            }
         }
-    }
+        return 1;
+    }();
 }
 class Solution {
 public:
+    Solution(){init();}
     vector<int> closestPrimes(int left, int right) {
-        if(!composite[0]) init();
         auto it1=lower_bound(prime.begin(),prime.end(),left);
         auto it2=upper_bound(prime.begin(),prime.end(),right)-1;
         if(it2-it1<1) return {-1,-1};
