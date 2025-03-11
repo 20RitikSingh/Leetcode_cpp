5d4c6edde4eefb2590d898a7db1b64250f02475d
Ritik Singh <92461786+20RitikSingh@users.noreply.github.com>
2025-03-11 04:35:08 -0700

    Time: 92 ms (5.72%) | Memory: 42.2 MB (5.04%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
@@ -1,24 +1,15 @@
 class Solution {
 public:
     int numberOfSubstrings(string s) {
-        int res = 0;
-        for (int i = 0; i < s.size(); i++) {
-            bool a = 0, b = 0, c = 0;
-            for (int j = i; j < s.size(); j++) {
-                if (s[j] == 'a')
-                    a = 1;
-                if (s[j] == 'b')
-                    b = 1;
-                if (s[j] == 'c')
-                    c = 1;
-                // if(v[j][a][b][c]>-1) return v[j][a][b][c];
-                if (a & b & c) {
-                    res += s.size() - j;
-                    break;
-                }
-            }
+        int res = 0,n=s.size();
+        vector<vector<int>> v(n,vector<int>(3,1e9));
+        for(int i=n-1;i>=0;i--){
+            if(i<n-1) v[i]=v[i+1];
+            v[i][s[i]-'a']=i;
         }
-
+        for (int i = 0; i < n; i++) {
+            res+=max(0,n-max(v[i][0],max(v[i][1],v[i][2])));
+        }
         return res;
     }
 };
