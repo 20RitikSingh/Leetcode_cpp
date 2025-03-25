1de89f33e196a0a07a2aab51853f2ed530c5b21f
Ritik Singh <92461786+20RitikSingh@users.noreply.github.com>
2025-03-25 00:25:45 -0700

    Time: 143 ms (16.61%) | Memory: 142.7 MB (15.23%) - LeetSync

--- 3430-count-days-without-meetings/count-days-without-meetings.cpp
+++ 3430-count-days-without-meetings/count-days-without-meetings.cpp
@@ -7,16 +7,11 @@ public:
         for (auto m : meetings) {
             if (v.back()[1] < m[0]) {
                 c+=m[0]-1-v.back()[1];
-                v.push_back(m);
+                v[0]=m;
             } else
                 v.back()[1] = max(v.back()[1], m[1]);
         }
         c+=days-v.back()[1];
-        // for (auto i : v)
-        //     cout << i[0] << "," << i[1] << " ";
         return c;
     }
 };
-// 0 1 2 3 4 5 6 7 8 9 10
-//   1  -1   1  -1   1 -1
-// 0 1 1 0 0 1 1 0 0 1 0
