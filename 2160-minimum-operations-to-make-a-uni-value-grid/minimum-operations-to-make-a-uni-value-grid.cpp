3411e79333a665ebd8d5a80f75befa48bed027fa
Ritik Singh <92461786+20RitikSingh@users.noreply.github.com>
2025-03-26 09:53:26 -0700

    Time: 60 ms (11.55%) | Memory: 84.2 MB (81.76%) - LeetSync

--- 2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.cpp
+++ 2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.cpp
@@ -3,9 +3,15 @@ public:
     int minOperations(vector<vector<int>>& grid, int x) {
         priority_queue<int> q;
         int n=grid.size()*grid[0].size()/2;
-        for(auto &i:grid) for(auto j:i) if(q.size()<=n) q.push(j); else if(q.top()>j){ q.pop(); q.push(j);}
+        for(int i=0;i<grid.size();i++) 
+            for(int &j:grid[i]) 
+                if(q.size()<=n) q.push(j); 
+                else if(q.top()>j){ 
+                    q.pop(); 
+                    q.push(j);
+                }
         int ans=0,c=q.top();
-        for(auto &i:grid)for(int j:i){
+        for(int i=0;i<grid.size();i++)for(int &j:grid[i]){
             if(abs(c-j)%x) return -1;
             ans+=abs(c-j)/x;        
         }
