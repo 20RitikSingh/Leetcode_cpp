3bad5a117d027f3d8d8bc607e4b1de38ad4de37a
Ritik Singh <92461786+20RitikSingh@users.noreply.github.com>
2025-03-26 09:34:50 -0700

    Time: 65 ms (9.73%) | Memory: 99.9 MB (6.38%) - LeetSync

---
+++ 2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minOperations(vector<vector<int>>& grid, int x) {
+        vector<int> v;
+        for(auto i:grid){
+            for(auto j:i) v.push_back(j);
+        }
+        sort(v.begin(),v.end());
+        int ans=0,c=v[v.size()/2];
+        for(int i:v){
+            if(abs(c-i)%x) return -1;
+            ans+=abs(c-i)/x;        
+        }
+        return ans;
+    }
+};
