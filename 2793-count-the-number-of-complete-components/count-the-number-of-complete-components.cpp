e4302b5bd8de9029073e6ce005b59e94b541cbf5
Ritik Singh <92461786+20RitikSingh@users.noreply.github.com>
2025-03-26 12:21:14 -0700

    Time: 122 ms (10.19%) | Memory: 157.8 MB (5.04%) - LeetSync

---
+++ 2793-count-the-number-of-complete-components/count-the-number-of-complete-components.cpp
@@ -0,0 +1,40 @@
+class Solution {
+    int dfs(vector<unordered_set<int>> &adj,vector<bool> &v,vector<bool> &vis, int curr){
+        // cout<<curr<<endl;
+        vis[curr]=1;
+        v[curr]=1;
+        bool x=1;
+        for(int i=0;i<vis.size();i++){
+            if(curr!=i && vis[i] && adj[curr].count(i)==0){ x=0; break;}
+        }
+        for(auto i:adj[curr]){
+            if(!v[i]){
+                x&=dfs(adj,v,vis,i);
+                // cout<<curr<<" returns "<<0<<endl;
+            }
+        }
+        // cout<<curr<<" returns "<<1<<endl;
+        return x;
+    }
+public:
+    int countCompleteComponents(int n, vector<vector<int>>& edges) {
+        vector<bool> v(n);
+        vector<unordered_set<int>> adj(n);
+        int c=0;
+        for(auto e:edges){
+            adj[e[0]].insert(e[1]);
+            adj[e[1]].insert(e[0]);
+        }
+        // for(int i=0;i<n;i++){
+        //     cout<<i<<": ";
+        //     for(int j:adj[i]) cout<<j<<" ";
+        //     cout<<endl;
+        // }
+        for(int i=0;i<n;i++){
+            if(v[i]) continue;
+            vector<bool> vis(n);
+            c+=dfs(adj,v,vis,i);
+        }
+        return c;
+    }
+};
