c3c73b5e26dd7dec0f605b4b8e932fa3c488d8d0
Ritik Singh <92461786+20RitikSingh@users.noreply.github.com>
2025-03-26 21:26:28 -0700

    Time: 39 ms (49.59%) | Memory: 98.4 MB (60.66%) - LeetSync

--- 2888-minimum-index-of-a-valid-split/minimum-index-of-a-valid-split.cpp
+++ 2888-minimum-index-of-a-valid-split/minimum-index-of-a-valid-split.cpp
@@ -2,21 +2,13 @@ class Solution {
 public:
     int minimumIndex(vector<int>& nums) {
         int n=nums.size(),curr=nums[0],c=0,k=0;
+        unordered_map<int,int> fq;
         for(int i=0;i<n;i++){
-            if(curr==nums[i]) c++;
-            else c--;
-            if(c<=0){
-                c=1;
-                curr=nums[i];
-            }
+            fq[nums[i]]++;
         }
-        c=0;
-        for(int i=0;i<n;i++) if(nums[i]==curr) c++;
-        // cout<<curr<<endl;
-        if(c<=n/2) return -1;
         for(int i=0;i<n;i++){
-            if(nums[i]==curr) k++;
-            if(k>(i+1)/2 && c-k>(n-1-i)/2) return i;
+            if(fq[nums[i]]*2>n) k++;
+            if(k>(i+1)/2 && fq[nums[i]]-k>(n-1-i)/2) return i;
         }
         return -1;
     }
