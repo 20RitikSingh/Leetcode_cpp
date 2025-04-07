472e8eec110f273d42a42ad5c4469729dc58de16
Ritik Singh <92461786+20RitikSingh@users.noreply.github.com>
2025-04-07 10:50:55 -0700

    Time: 58 ms (88.56%) | Memory: 16.3 MB (78.01%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -4,12 +4,12 @@ public:
         int n=nums.size(),sum=0;
         for(int i:nums) sum+=i;
         if(sum%2) return 0;
-        vector<bool> v(20001,0);
+        vector<bool> v(sum/2+1,0);
         v[0]=1;
         for(int i=0;i<n;i++){
             vector<bool> u=v;
-            for(int j=0;j<20001;j++){
-                if(v[j]) u[nums[i]+j]=1;
+            for(int j=0;j<sum/2+1;j++){
+                if(v[j] && nums[i]+j<sum/2+1) u[nums[i]+j]=1;
             }
             v=u;
         }
