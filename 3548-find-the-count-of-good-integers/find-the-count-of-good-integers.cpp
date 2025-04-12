79d0dba113768a799fe4e8a7c14d5cac9e2d2d98
Ritik Singh <92461786+20RitikSingh@users.noreply.github.com>
2025-04-12 11:16:18 -0700

    Time: 89 ms (84.48%) | Memory: 16.1 MB (89.66%) - LeetSync

--- 3548-find-the-count-of-good-integers/find-the-count-of-good-integers.cpp
+++ 3548-find-the-count-of-good-integers/find-the-count-of-good-integers.cpp
@@ -1,23 +1,13 @@
 class Solution {
     long long pow(long long x,long long y){
         if(y==0) return 1;
-        if(y==1) return x;
         long long k=pow(x,y/2);
         if(y%2) return x*k*k;
         return k*k;
     }
-    long long pal(int x){
+    long long pal(int x,int p){
         long long res=x;
-        while(x>0){
-            res*=10;
-            res+=x%10;
-            x/=10;
-        }
-        return res; 
-    }
-    long long palodd(int x){
-        long long res=x;
-        x/=10;
+        x/=pow(10,p);
         while(x>0){
             res*=10;
             res+=x%10;
@@ -37,15 +27,11 @@ class Solution {
         }
         if(set.count(s)) return 0;
         set.insert(s);
-        // cout<<s<<endl;
         long long res=0;
         for(int i=1;i<10;i++){
             long long x=fact[d-1];
             if(s[i]==0) continue;
-            for(int j=0;j<10;j++){
-                if(j==i) x/=fact[s[j]-1];
-                else x/=fact[s[j]];
-            }
+            for(int j=0;j<10;j++) x/=fact[s[j]-(i==j)];
             res+=x;
         }
         return res;
@@ -53,30 +39,10 @@ class Solution {
 public:
     long long countGoodIntegers(int n, int k) {
         for(int i=1;i<11;i++) fact[i]=fact[i-1]*i;
-        if(n%2){
-            int d=n/2;
-            int x=pow(10,d);
-            long long c=0;
-            for(int i=0;i+x<pow(10,d+1);i++){
-                long long t=palodd(i+x);
-                if(t%k==0){
-                    c+=perm(t);
-                    // cout<<t<<endl;
-                }
-            }
-            return c;
-        }
-        int d=n/2;
-        int x=pow(10,d-1);
-        long long c=0;
-        // cout<<x<<endl;
-        for(int i=0;i+x<pow(10,d);i++){
-            long long t=pal(i+x);
-            if(t%k==0){
-                c+=perm(t);
-                // cout<<t<<endl;
-                // cout<<perm(t)<<endl;
-            }
+        long long c=0,d=n/2,x=pow(10,d-1+n%2);
+        for(int i=0;i+x<pow(10,d+n%2);i++){
+            long long t=pal(i+x,n%2);
+            if(t%k==0) c+=perm(t);
         }
         return c;
     }
