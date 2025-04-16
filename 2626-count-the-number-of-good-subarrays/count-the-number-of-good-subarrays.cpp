d722432401b572170b967f17325dda02d3fa42c8
Ritik Singh <92461786+20RitikSingh@users.noreply.github.com>
2025-04-17 01:49:26 +0530

    Time: 74 ms (85.58%) | Memory: 79.2 MB (35.93%) - LeetSync

--- 2626-count-the-number-of-good-subarrays/count-the-number-of-good-subarrays.cpp
+++ 2626-count-the-number-of-good-subarrays/count-the-number-of-good-subarrays.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     long long countGood(vector<int>& nums, int k) {
         unordered_map<int,int> mp;
-        int f=1,b=0,n=nums.size(),c=0,p=0;
+        int f=1,b=0,n=nums.size(),c=0,p=0,old=-1;
         vector<pair<int,int>> v;
         long long res=0;
         mp[nums[0]]++;
@@ -18,12 +18,9 @@ public:
                 p-=mp[nums[b]];
                 b++;
             }
-            v.push_back({b-1,f-1});
-            // cout<<b-1<<" "<<f-1<<endl;
-        }
-        for(int i=0;i<v.size();i++){
-            // cout<<v[i].first<<" "<<v[i].second<<endl;
-            res+=1LL*(v[i].first-((i==0)?-1:v[i-1].first))*(n-v[i].second);
+            // cout<<b-1<<" "<<f-1<<" "<<old<<endl;
+            res+=1LL*(b-1-old)*(n-f+1);
+            old=b-1;
         }
         return res;
     }
