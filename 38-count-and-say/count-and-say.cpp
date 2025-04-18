ef9bf6572a95c6c2d0deb68497044d1b0e50aa74
Ritik Singh <92461786+20RitikSingh@users.noreply.github.com>
2025-04-18 11:55:54 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (97.27%) - LeetSync

--- 38-count-and-say/count-and-say.cpp
+++ 38-count-and-say/count-and-say.cpp
@@ -1,21 +1,23 @@
 class Solution {
 public:
     string countAndSay(int n) {
-        if(n==1) return "1";
-        string res="",s=countAndSay(n-1);
-        // cout<<n-1<<" "<<s<<endl;
-        int c=1,e=s[0];
-        for(int i=1;i<s.size();i++){
-            if(e==s[i]) c++;
-            else{
-                res.push_back(c+'0');
-                res.push_back(e);
-                c=1;
-                e=s[i];
+        string s="1";
+        for(int k=1;k<n;k++){
+            string res="";
+            int c=1,e=s[0];
+            for(int i=1;i<s.size();i++){
+                if(e==s[i]) c++;
+                else{
+                    res.push_back(c+'0');
+                    res.push_back(e);
+                    c=1;
+                    e=s[i];
+                }
             }
+            res.push_back(c+'0');
+            res.push_back(e);
+            s=res;
         }
-        res.push_back(c+'0');
-        res.push_back(e);
-        return res;
+        return s;
     }
 };
