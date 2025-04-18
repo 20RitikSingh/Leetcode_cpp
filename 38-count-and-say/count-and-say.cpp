8991bf67cff9c8a1dd3d42eaf0b7bb23d9b703df
Ritik Singh <92461786+20RitikSingh@users.noreply.github.com>
2025-04-18 12:08:40 +0530

    Time: 55 ms (6.77%) | Memory: 124.2 MB (6.46%) - LeetSync

--- 38-count-and-say/count-and-say.cpp
+++ 38-count-and-say/count-and-say.cpp
@@ -1,23 +1,15 @@
 class Solution {
+    string RLE(string s){
+        string res="1"+string(1,s[0]);
+        for(int i=1;i<s.size();i++)
+            if(res.back()==s[i]) res[res.size()-2]++;
+            else res=res+"1"+string(1,s[i]);
+        return res;
+    }
 public:
     string countAndSay(int n) {
         string s="1";
-        for(int k=1;k<n;k++){
-            string res="";
-            int c=1,e=s[0];
-            for(int i=1;i<s.size();i++){
-                if(e==s[i]) c++;
-                else{
-                    res.push_back(c+'0');
-                    res.push_back(e);
-                    c=1;
-                    e=s[i];
-                }
-            }
-            res.push_back(c+'0');
-            res.push_back(e);
-            s=res;
-        }
+        for(int k=1;k<n;k++) s=RLE(s);
         return s;
     }
 };
