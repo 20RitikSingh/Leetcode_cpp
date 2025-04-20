27513f3933d477bea4b936cc8e76ff85b6099c58
Ritik Singh <92461786+20RitikSingh@users.noreply.github.com>
2025-04-20 11:56:29 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (10.59%) - LeetSync

--- 797-rabbits-in-forest/rabbits-in-forest.cpp
+++ 797-rabbits-in-forest/rabbits-in-forest.cpp
@@ -1,13 +1,12 @@
 class Solution {
 public:
     int numRabbits(vector<int>& answers) {
-        vector<int> v(1001);
-        int res=0;
-        for(int i:answers) v[i]++;
+        int v[1001];
+        int res=0,n=answers.size();
+        for(int i=0;i<n;i++) v[answers[i]]++;
         for(int i=1;i<1001;i++){
             if(v[i]){ 
-                res+=(i+1)*(v[i]/(i+1));
-                if(v[i]%(i+1)) res+=i+1;
+                res+=v[i]+(i+1-v[i]%(i+1))%(i+1);
             }
         }
         return res+v[0];
