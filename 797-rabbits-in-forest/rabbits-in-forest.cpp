87c4be6ddc5416ae108797b157fe6975a2146bb4
Ritik Singh <92461786+20RitikSingh@users.noreply.github.com>
2025-04-20 12:28:07 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (99.42%) - LeetSync

--- 797-rabbits-in-forest/rabbits-in-forest.cpp
+++ 797-rabbits-in-forest/rabbits-in-forest.cpp
@@ -1,6 +1,6 @@
 struct Solution{
     int numRabbits(vector<int>& answers) {
-        int16_t v[1001];
+        int8_t v[1001];
         int res=0;
         for(int i:answers) if(!i || ++v[i]%(i+1)==1) res+=(i+1);
         return res;
