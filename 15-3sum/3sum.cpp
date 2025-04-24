cfe099b8cc7048aae3600efe5c67dc833a11e97f
Ritik Singh <92461786+20RitikSingh@users.noreply.github.com>
2025-04-24 22:53:29 +0530

    Time: 2253 ms (5.00%) | Memory: 472.7 MB (5.01%) - LeetSync

---
+++ 15-3sum/3sum.cpp
@@ -0,0 +1,31 @@
+class Solution {
+    int twoSum(vector<int> &nums,int idx,vector<vector<int>> &res){
+        cout<<nums[idx]<<endl;
+        unordered_set<int> set;
+        int n=nums.size();
+        int i=idx;
+        i++;
+        for(;i<n;i++){
+            // if(i==idx) continue;
+            if(set.count(0-nums[i]-nums[idx])){
+                res.push_back({nums[idx],nums[i],0-nums[i]-nums[idx]});
+                while(i<n-1 && nums[i]==nums[i+1]) i++;
+            }
+            set.insert(nums[i]);
+        }
+        return 0;
+    }
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> res;
+        int n=nums.size();
+        sort(nums.begin(),nums.end());
+        // for(int i:nums) cout<<i<<" ";
+        // cout<<endl;
+        for(int i=0;i<n;i++){
+            twoSum(nums,i,res);
+            while(i<n-1 && nums[i]==nums[i+1]) i++;
+        }
+        return res;
+    }
+};
