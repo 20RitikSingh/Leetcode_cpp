0fda57f4d2b6ba4432749efd8241a84fbe1615aa
Ritik Singh <92461786+20RitikSingh@users.noreply.github.com>
2025-05-04 17:03:14 +0530

    Time: 5 ms (98.43%) | Memory: 52.7 MB (81.15%) - LeetSync

--- 3833-merge-operations-for-minimum-travel-time/merge-operations-for-minimum-travel-time.cpp
+++ 3833-merge-operations-for-minimum-travel-time/merge-operations-for-minimum-travel-time.cpp
@@ -3,31 +3,19 @@ class Solution {
     int rec(vector<int> &d,vector<int> &t,int k,int idx,int prev){
         if(idx>=d.size()) return k?1e9:0;
         if(dp[k][idx][prev]) return dp[k][idx][prev];
-        int c=d[idx]*(t[idx]+prev);
-        int mi=rec(d,t,k,idx+1,0)+c;
-        int s=0,p=0;
+        int c=d[idx]*(t[idx]+prev),s=0,p=0;
+        dp[k][idx][prev]=rec(d,t,k,idx+1,0)+c;
         for(int i=1;i+idx<d.size() && k-i>=0;i++){
             p+=d[i+idx]*(t[idx]+prev);
             s+=t[i+idx];
-            // cout<<d[i+idx]<<" ";
-            mi=min(mi,rec(d,t,k-i,idx+i+1,s)+p+c);
-            // cout<<endl;
+            dp[k][idx][prev]=min(dp[k][idx][prev],rec(d,t,k-i,idx+i+1,s)+p+c);
         }
-        dp[k][idx][prev]=mi;
-        return mi;
+        return dp[k][idx][prev];
     }
 public: 
     int minTravelTime(int l, int n, int k, vector<int>& position, vector<int>& time) {
-        vector<int> d,t;
-        for(int i=1;i<n;i++){
-            d.push_back(position[i]-position[i-1]);
-        }
-        t=time;
-        t.pop_back();
-        // for(int i:d) cout<<i<<" ";
-        // cout<<endl;
-        // for(int i:t) cout<<i<<" ";
-        // cout<<endl;
+        vector<int> d,t=time;
+        for(int i=1;i<n;i++) d.push_back(position[i]-position[i-1]);
         return rec(d,t,k,0,0);
     }
 };
