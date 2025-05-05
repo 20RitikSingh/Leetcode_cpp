b7920b9d78783bc989fbeed72c7efb4056e3d992
Ritik Singh <92461786+20RitikSingh@users.noreply.github.com>
2025-05-05 17:29:12 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (45.99%) - LeetSync

--- 806-domino-and-tromino-tiling/domino-and-tromino-tiling.cpp
+++ 806-domino-and-tromino-tiling/domino-and-tromino-tiling.cpp
@@ -1,34 +1,14 @@
-class mod {
-    static const int MOD = 1000000007;
-public:
-    int val;
-    mod(int x = 0) {
-        val = ((x % MOD) + MOD) % MOD;
-    }
-    mod operator+(const mod &other) const {
-        return mod((val + other.val) % MOD);
-    }
-    mod operator-(const mod &other) const {
-        return mod(((val - other.val + MOD) % MOD));
-    }
-    mod operator*(const mod &other) const {
-        return mod((1LL * val * other.val) % MOD);
-    }
-    operator int() const{
-        return val;
-    }
-};
-
 class Solution {
 public:
     int numTilings(int n) {
-        vector<mod> top(n+5),full(n+5);
+        int mod=1e9+7;
+        vector<int> top(n+5),full(n+5);
         top[2]=1;
         full[1]=1;
         full[2]=2;
         for(int i=3;i<=n;i++){
-            top[i]=top[i-1]+full[i-2];
-            full[i]=full[i-1]+full[i-2]+top[i-1]+top[i-1];
+            top[i]=(1LL*top[i-1]+full[i-2])%mod;
+            full[i]=(1LL*full[i-1]+full[i-2]+top[i-1]+top[i-1])%mod;
         }
         return full[n];
     }
