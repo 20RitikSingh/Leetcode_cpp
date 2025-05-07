55c4ce671d8bd4710fe077f633c78f170baa7dd8
Ritik Singh <92461786+20RitikSingh@users.noreply.github.com>
2025-05-07 17:03:53 +0530

    Time: 25 ms (43.95%) | Memory: 32.8 MB (21.36%) - LeetSync

--- 3627-find-minimum-time-to-reach-last-room-i/find-minimum-time-to-reach-last-room-i.cpp
+++ 3627-find-minimum-time-to-reach-last-room-i/find-minimum-time-to-reach-last-room-i.cpp
@@ -11,21 +11,15 @@ public:
         q.push({0,0,0});
         vector<vector<int>> cost(moveTime.size(),vector<int>(moveTime[0].size(),2e9));
         cost[0][0]=0;
+        int dx[4]={0,0,1,-1};
+        int dy[4]={1,-1,0,0};
         while(q.size()){
             auto curr=q.top();
             q.pop();
-            // cout<<curr[0]<<":"<<curr[1]<<":"<<curr[2]<<endl;
-            int dx[4]={0,0,1,-1};
-            int dy[4]={1,-1,0,0};
             for(int i=0;i<4;i++){
                 int x=curr[1]+dx[i];
                 int y=curr[2]+dy[i];
                 if(x<0 || y<0 || x>=moveTime.size() || y>=moveTime[0].size() || cost[x][y]<1e9) continue;
-                // cout<<x<<","<<y<<endl;
-                // for(auto i:cost){
-                //     for(int j:i) cout<<j<<" ";
-                //     cout<<endl;
-                // }
                 int newCost=max(moveTime[x][y],curr[0])+1;
                 if(x==moveTime.size()-1 && y==moveTime[0].size()-1) return newCost;
                 if(newCost<cost[x][y]){ 
