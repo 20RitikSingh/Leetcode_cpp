2ab924f9fac694fb444ba51e280f8aa9a4ad8179
Ritik Singh <92461786+20RitikSingh@users.noreply.github.com>
2025-05-07 15:48:52 +0530

    Time: 597 ms (28.20%) | Memory: 51.4 MB (51.23%) - LeetSync

---
+++ 3841-concatenated-divisibility/concatenated-divisibility.cpp
@@ -0,0 +1,42 @@
+class Solution {
+    int pow(int x,int y,int m){
+        if(!y) return 1; 
+        int r=pow(x,y/2,m);
+        if(y%2) return (((1LL*r*r)%m)*x)%m;
+        else return (1LL*r*r)%m;
+    }
+    int digits(int n){
+        int c=0;
+        for(;n>0;c++,n/=10);
+        return c;
+    }
+    vector<int> v;
+    int dp[65000][101];
+    int rec(vector<int> &nums,int &msk,int k,int r){
+        if(dp[msk][r]) return dp[msk][r];
+        bool f=0;
+        for(int i=0;i<nums.size();i++){
+            if(msk&(1<<i)) continue;
+            f=1;
+            msk=msk|(1<<i);
+            int x=(1LL*r*pow(10,digits(nums[i]),k)+nums[i])%k;
+            if(rec(nums,msk,k,x)==0){
+                v.push_back(nums[i]);
+                return 0;
+            }
+            msk=msk^(1<<i);
+        }
+        if(f){dp[msk][r]=1; return 1;}
+        dp[msk][r]=r;
+        return r;
+    }
+public:
+    vector<int> concatenatedDivisibility(vector<int>& nums, int k) {
+        sort(nums.begin(),nums.end());
+        int msk=0;
+        rec(nums,msk,k,0);
+        if(v.size()<nums.size()) v.clear();
+        reverse(v.begin(),v.end());
+        return v;
+    }
+};
