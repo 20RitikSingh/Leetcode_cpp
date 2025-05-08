f54aa9db02acb6b21811cb62e1e6f1c01b933bd3
Ritik Singh <92461786+20RitikSingh@users.noreply.github.com>
2025-05-08 13:56:30 +0530

    Time: 39 ms (20.81%) | Memory: 32.7 MB (20.46%) - LeetSync

--- 3627-find-minimum-time-to-reach-last-room-i/find-minimum-time-to-reach-last-room-i.cpp
+++ 3627-find-minimum-time-to-reach-last-room-i/find-minimum-time-to-reach-last-room-i.cpp
@@ -1,16 +1,15 @@
-class comp{
-public:
-bool operator()(vector<int> &a, vector<int> &b){
-    return a[0]>b[0];
-}
+class cmp{
+    public:
+    bool operator()(vector<int> &a, vector<int> &b){
+        return a[0]>b[0];
+    }
 };
 class Solution {
 public:
     int minTimeToReach(vector<vector<int>>& moveTime) {
-        priority_queue<vector<int>,vector<vector<int>>, comp> q;
+        priority_queue<vector<int>,vector<vector<int>>,cmp> q;
         q.push({0,0,0});
         vector<vector<int>> cost(moveTime.size(),vector<int>(moveTime[0].size(),2e9));
-        cost[0][0]=0;
         int dx[4]={0,0,1,-1};
         int dy[4]={1,-1,0,0};
         while(q.size()){
@@ -19,15 +18,15 @@ public:
             for(int i=0;i<4;i++){
                 int x=curr[1]+dx[i];
                 int y=curr[2]+dy[i];
-                if(x<0 || y<0 || x>=moveTime.size() || y>=moveTime[0].size() || cost[x][y]<1e9) continue;
-                int newCost=max(moveTime[x][y],curr[0])+1;
+                if(x<0 || y<0 || x>=moveTime.size() || y>=moveTime[0].size() || cost[x][y]<2e9) continue;
+                int newCost=max(curr[0],moveTime[x][y])+1;
                 if(x==moveTime.size()-1 && y==moveTime[0].size()-1) return newCost;
-                if(newCost<cost[x][y]){ 
+                if(newCost<cost[x][y]){
                     q.push({newCost,x,y});
                     cost[x][y]=newCost;
                 }
             }
         }
-        return 2e9;
+        return -1;
     }
 };
