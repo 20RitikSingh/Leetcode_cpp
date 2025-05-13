fd1002bac69279ea96e02e398b48e0920dea0fc2
Ritik Singh <92461786+20RitikSingh@users.noreply.github.com>
2025-05-13 17:51:45 +0530

    Time: 3 ms (99.51%) | Memory: 19.3 MB (76.35%) - LeetSync

--- 3629-total-characters-in-string-after-transformations-i/total-characters-in-string-after-transformations-i.cpp
+++ 3629-total-characters-in-string-after-transformations-i/total-characters-in-string-after-transformations-i.cpp
@@ -1,14 +1,13 @@
-int dp[100001],mod=1e9+7;
 long long rec(int t){
+    static int dp[100001];
     if(t<0) return 1;
-    if(dp[t]==0) dp[t]=(dp[t]+rec(t-26)+rec(t-25))%mod;
+    if(dp[t]==0) dp[t]=(dp[t]+rec(t-26)+rec(t-25))%1000000007;
     return dp[t];
 }
 class Solution {
 public:
-    int lengthAfterTransformations(string s, int t) {
-        int res=0;
-        for(char ch:s) res=(res+rec(t-'z'+ch-1))%mod;
+    int lengthAfterTransformations(string s, int t,int res=0) {
+        for(char ch:s) res=(res+rec(t-'z'+ch-1))%1000000007;
         return res;
     }
 };
