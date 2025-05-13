3eb7b24bebc051fa8366b8f7157b0e751cdc168a
Ritik Singh <92461786+20RitikSingh@users.noreply.github.com>
2025-05-13 18:00:02 +0530

    Time: 5 ms (99.51%) | Memory: 19.4 MB (68.97%) - LeetSync

--- 3629-total-characters-in-string-after-transformations-i/total-characters-in-string-after-transformations-i.cpp
+++ 3629-total-characters-in-string-after-transformations-i/total-characters-in-string-after-transformations-i.cpp
@@ -1,11 +1,10 @@
 long long rec(int t){
     static int dp[100001];
     if(t<0) return 1;
-    if(dp[t]==0) dp[t]=(dp[t]+rec(t-26)+rec(t-25))%1000000007;
+    if(!dp[t]) dp[t]=(dp[t]+rec(t-26)+rec(t-25))%1000000007;
     return dp[t];
 }
-class Solution {
-public:
+struct Solution {
     int lengthAfterTransformations(string s, int t,int res=0) {
         for(char ch:s) res=(res+rec(t-'z'+ch-1))%1000000007;
         return res;
