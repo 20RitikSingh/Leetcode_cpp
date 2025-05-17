ce80362199c8520b7e11ea4e80c120336dccd118
Ritik Singh <92461786+20RitikSingh@users.noreply.github.com>
2025-05-18 04:41:25 +0530

    Time: 25 ms (10.09%) | Memory: 22.1 MB (5.05%) - LeetSync

--- 1197-parsing-a-boolean-expression/parsing-a-boolean-expression.cpp
+++ 1197-parsing-a-boolean-expression/parsing-a-boolean-expression.cpp
@@ -1,30 +1,42 @@
 class Solution {
 public:
     bool parseBoolExpr(string exp) {
-        stack<char> st;
-        for(char ch:exp){
-            if(ch==',') continue;
-            if(ch==')'){
-                bool aand=1,oor=0;
-                while(st.top()!='('){
-                    aand&=st.top()=='t'?1:0;
-                    oor|=st.top()=='t'?1:0;
-                    st.pop();
+        if(exp=="t") return 1;
+        if(exp=="f") return 0;
+        bool f=0;
+        if(exp[0]=='&') f=1;
+        string t;
+        int i=2,c=0;
+        while(i<exp.size()-1){
+            t.push_back(exp[i]);
+            if(exp[i]=='(') c++;
+            else if(exp[i]==')') c--;
+            if(!c && exp[i]==','){
+                t.pop_back();
+                switch(exp[0]){
+                case '&':
+                    f&=parseBoolExpr(t);
+                    break;
+                case '|':
+                    f|=parseBoolExpr(t);
+                    break;
+                default:
+                    return !parseBoolExpr(t);
                 }
-                st.pop();
-                char x=st.top();
-                st.pop();
-                if(x=='!'){
-                    st.push(oor?'f':'t');
-                }else if(x=='&'){
-                    st.push(aand?'t':'f');
-                }else if(x=='|'){
-                    st.push(oor?'t':'f');
-                }
-                continue;
-            }
-            st.push(ch);
+                t="";
+            } 
+            i++;
         }
-        return st.top()=='t'?1:0;
+        if(t.size())switch(exp[0]){
+        case '&':
+            f&=parseBoolExpr(t);
+            break;
+        case '|':
+            f|=parseBoolExpr(t);
+            break;
+        default:
+            return !parseBoolExpr(t);
+        }
+        return f;
     }
 };
