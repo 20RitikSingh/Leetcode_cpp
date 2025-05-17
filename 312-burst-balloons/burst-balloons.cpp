2b930a55125965da21ce77ba49d24851cc44c262
Ritik Singh <92461786+20RitikSingh@users.noreply.github.com>
2025-05-18 03:49:32 +0530

    Time: 147 ms (55.44%) | Memory: 10.9 MB (98.29%) - LeetSync

--- 312-burst-balloons/burst-balloons.cpp
+++ 312-burst-balloons/burst-balloons.cpp
@@ -1,21 +1,18 @@
 class Solution {
     int dp[302][302];
-    int rec(vector<int> &nums,int l,int h){
+    vector<int> nums={1};
+    int rec(int l,int h){
+        if(l==h) return 0;
         if(h-l==1) return nums[l-1]*nums[l]*nums[h];
-        if(dp[l][h]!=-1) return dp[l][h];
+        if(dp[l][h]) return dp[l][h];
         dp[l][h]=0;
-        for(int i=l;i<h;i++){
-            dp[l][h]=max(dp[l][h],rec(nums,l,i)+nums[l-1]*nums[i]*nums[h]+rec(nums,i+1,h));
-        }
+        for(int i=l;i<h;i++) dp[l][h]=max(dp[l][h],rec(l,i)+nums[l-1]*nums[i]*nums[h]+rec(i+1,h));
         return dp[l][h];
     }
 public:
-    int maxCoins(vector<int>& nums) {
-        int n=nums.size();
-        vector<int> v={1};
-        memset(dp,-1,sizeof(dp));
-        for(int i:nums) v.push_back(i);
-        v.push_back(1);
-        return rec(v,1,n+1);
+    int maxCoins(vector<int>& v) {
+        for(int i:v) nums.push_back(i);
+        nums.push_back(1);
+        return rec(1,v.size()+1);
     }
 };
