514db54ced4b593c0d137e29ef19d42e9c870e45
Ritik Singh <92461786+20RitikSingh@users.noreply.github.com>
2025-05-17 10:04:24 +0530

    Time: 16 ms (95.33%) | Memory: 12.1 MB (72.89%) - LeetSync

--- 972-knight-dialer/knight-dialer.cpp
+++ 972-knight-dialer/knight-dialer.cpp
@@ -1,35 +1,45 @@
-class Solution {
-    vector<vector<int>> adj={
-        {4,6},
-        {8,6},
-        {7,9},
-        {4,8},
-        {0,3,9},
-        {},
-        {0,1,7},
-        {2,6},
-        {1,3},
-        {2,4}
-    };
-    int mod=1e9+7;
-    int dp[10][5001];
-    int rec(int curr,int n){
-        if(n<=0) return 1;
-        if(dp[curr][n]!=0) return dp[curr][n];
-        long long res=0;
-        for(int i:adj[curr]){
-            res=(res+rec(i,n-1))%mod;
+vector<vector<int>> adj = {{4, 6}, {8, 6},    {7, 9}, {4, 8}, {0, 3, 9},
+                           {},     {0, 1, 7}, {2, 6}, {1, 3}, {2, 4}};
+int mod = 1e9 + 7;
+vector<vector<int>> matMul(vector<vector<int>>& x, vector<vector<int>>& y) {
+    int mx = x.size(), nx = x[0].size(), my = y.size(), ny = y[0].size();
+    if (nx != my)
+        return {{}};
+    vector<vector<int>> res(mx, vector<int>(ny));
+    for (int i = 0; i < mx; i++) {
+        for (int j = 0; j < ny; j++) {
+            for (int k = 0; k < nx; k++) {
+                res[i][j] = (res[i][j] + 1LL * x[i][k] * y[k][j]) % mod;
+            }
         }
-        dp[curr][n]=res;
-        return res;
     }
+    return res;
+}
+vector<vector<int>> matPow(vector<vector<int>>& x, int p) {
+    if (x.size() != x[0].size())
+        return {{}};
+    if (p == 1)
+        return x;
+    vector<vector<int>> tmp = matPow(x, p / 2);
+    tmp = matMul(tmp, tmp);
+    if (p % 2)
+        tmp = matMul(tmp, x);
+    return tmp;
+}
+class Solution {
 public:
     int knightDialer(int n) {
-        long long res=0;
-        for(int i=0;i<10;i++){
-            res+=rec(i,n-1);
-            res%=mod;
-        }
-        return res;
+        if (n < 2)
+            return 10;
+        long long res = 0;
+        vector<vector<int>> mat(10, vector<int>(10)), dp(1, vector<int>(10, 1)),
+            tmp(10, vector<int>(1, 1));
+        for (int i = 0; i < 10; i++)
+            for (int j : adj[i])
+                mat[i][j] = 1;
+        mat = matPow(mat, n - 1);
+        dp = matMul(dp, mat);
+        tmp = matMul(dp, tmp);
+        return tmp[0][0];
     }
 };
