3d7198ed59e5df503fc80c81387d9270a6ba55ba
Ritik Singh <92461786+20RitikSingh@users.noreply.github.com>
2025-05-20 01:28:21 +0530

    Time: 23 ms (78.67%) | Memory: 54 MB (12.31%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -1,17 +1,30 @@
 class Solution {
     int dp[1000][1000];
+    int nxt[1000][100];
     int rec(string s,string t, int i,int j){
         if(j==t.size()) return 1;
         if(i==s.size()) return 0;
         if(t.size()-j>s.size()-i) return 0;
         if(dp[i][j]!=-1) return dp[i][j];
-        dp[i][j]=rec(s,t,i+1,j);
+        int k=nxt[i][t[j]-'A'];
+        dp[i][j]=0;
+        if(k!=0) dp[i][j]+=rec(s,t,k,j);
         if(s[i]==t[j]) dp[i][j]+=rec(s,t,i+1,j+1);
         return dp[i][j];
     }
 public:
     int numDistinct(string s, string t) {
         memset(dp,-1,sizeof(dp));
+        for(int i=s.size()-2;i>=0;i--){
+            for(int j=0;j<100;j++) nxt[i][j]=nxt[i+1][j];
+            nxt[i][s[i+1]-'A']=i+1;
+        }
+        // for(int i=0;i<s.size();i++){
+        //     for(int j=0;j<26;j++){
+        //         cout<<nxt[i][j]<<" ";
+        //     }
+        //     cout<<endl;
+        // }
         return rec(s,t,0,0);
     }
 };
