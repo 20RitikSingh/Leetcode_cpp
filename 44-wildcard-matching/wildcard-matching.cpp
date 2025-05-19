cbf9b3f62ba20c00c450172609982ad430a5fd20
Ritik Singh <92461786+20RitikSingh@users.noreply.github.com>
2025-05-20 03:03:32 +0530

    Time: 38 ms (27.16%) | Memory: 12.3 MB (82.93%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.cpp
+++ 44-wildcard-matching/wildcard-matching.cpp
@@ -1,20 +1,16 @@
 class Solution {
 public:
     bool isMatch(string s, string t) {
-        if(s.empty()){
-            for(char ch:t) if(ch!='*') return 0;
-            return 1;
-        }
-        vector<vector<bool>> dp(s.size()+1,vector<bool>(t.size()+1,0));
-        dp[s.size()][t.size()]=1;
-        for(int j=t.size()-1;j>=0;j--){
-            dp[s.size()][j]=(t[j]=='*' && dp[s.size()][j+1]);
-        }
+        vector<vector<bool>> dp(2,vector<bool>(t.size()+1,0));
+        dp[0][t.size()]=1;
+        for(int j=t.size()-1;j>=0;j--) dp[0][j]=(t[j]=='*' && dp[0][j+1]);
         for(int i=s.size()-1;i>=0;i--){
             for(int j=t.size()-1;j>=0;j--){
-                dp[i][j]=((s[i]==t[j] || t[j]=='?') && dp[i+1][j+1])
-                        ||(t[j]=='*' && (dp[i+1][j] || dp[i][j+1]));
+                dp[1][j]=((s[i]==t[j] || t[j]=='?') && dp[0][j+1])
+                        ||(t[j]=='*' && (dp[0][j] || dp[1][j+1]));
             }
+            dp[0]=dp[1];
+            dp[1]=vector<bool>(t.size());
         }
         return dp[0][0];
     }
