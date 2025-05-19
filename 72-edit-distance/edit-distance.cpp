905e3f5f8116157d26ea8a7dc8b4a50d68d75890
Ritik Singh <92461786+20RitikSingh@users.noreply.github.com>
2025-05-20 02:09:11 +0530

    Time: 812 ms (5.00%) | Memory: 399.1 MB (5.70%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,14 +1,9 @@
 class Solution {
     int dp[500][500];
-    int rec(string s,string t,int i,int j){
-        if(i==s.size()) return t.size()-j;
-        if(j==t.size()) return s.size()-i;
-        if(!dp[i][j]) 
-        dp[i][j]=min(rec(s,t,i+1,j+1)+(s[i]!=t[j]),min(rec(s,t,i+1,j)+1,rec(s,t,i,j+1)+1));
-        return dp[i][j];
-    }
 public:
-    int minDistance(string word1, string word2) {
-        return rec(word1,word2,0,0);
+    int minDistance(string s, string t,int i=0,int j=0) {
+        if(i==s.size() || j==t.size()) return max(s.size()-i,t.size()-j);
+        if(!dp[i][j]) dp[i][j]=min(minDistance(s,t,i+1,j+1)+(s[i]!=t[j]),min(minDistance(s,t,i+1,j)+1,minDistance(s,t,i,j+1)+1));
+        return dp[i][j];
     }
 };
