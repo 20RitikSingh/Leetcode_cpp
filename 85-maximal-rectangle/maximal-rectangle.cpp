f090f254470fe3556f78892bf090d6da7cf3e1d3
Ritik Singh <92461786+20RitikSingh@users.noreply.github.com>
2025-05-20 07:04:59 +0530

    Time: 6 ms (69.31%) | Memory: 18.6 MB (55.63%) - LeetSync

--- 85-maximal-rectangle/maximal-rectangle.cpp
+++ 85-maximal-rectangle/maximal-rectangle.cpp
@@ -3,24 +3,28 @@ public:
     int maximalRectangle(vector<vector<char>>& grid) {
         int res = 0, n = grid.size(), m = grid[0].size();
         vector<int> nxt(m, n); 
+        vector<int> v(m), prevmin(m);
+        stack<int> st;
 
         for (int k = n - 1; k >= 0; k--) {
-            vector<int> v(m);
-            stack<int> st;
-            vector<int> prevmin(m, -1);
+            // Compute heights and previous smaller in one pass
+            st = stack<int>();  // Clear stack
             for (int j = 0; j < m; j++) {
                 if (grid[k][j] == '0') nxt[j] = k;
                 v[j] = nxt[j] - k;
+
                 while (!st.empty() && v[st.top()] >= v[j]) st.pop();
-                if (!st.empty()) prevmin[j] = st.top();
+                prevmin[j] = st.empty() ? -1 : st.top();
                 st.push(j);
             }
-            st=stack<int>(); 
-            for (int i = m - 1, nxtmin = m; i >= 0; i--) {
-                while (!st.empty() && v[st.top()] >= v[i]) st.pop();
-                if (!st.empty()) nxtmin = st.top();
-                res = max(res, (nxtmin - prevmin[i] - 1) * v[i]);
-                st.push(i);
+
+            // Compute next smaller and max area
+            st = stack<int>();  // Clear stack
+            for (int j = m - 1, nxtmin = m; j >= 0; j--) {
+                while (!st.empty() && v[st.top()] >= v[j]) st.pop();
+                nxtmin = st.empty() ? m : st.top();
+                res = max(res, (nxtmin - prevmin[j] - 1) * v[j]);
+                st.push(j);
             }
         }
 
