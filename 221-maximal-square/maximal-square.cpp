7b9ece80e7a461910e906bc7bb67281153494860
Ritik Singh <92461786+20RitikSingh@users.noreply.github.com>
2025-05-22 01:38:17 +0530

    Time: 30 ms (15.03%) | Memory: 39.8 MB (5.09%) - LeetSync

---
+++ 221-maximal-square/maximal-square.cpp
@@ -0,0 +1,37 @@
+class Solution {
+    int rec(vector<int>& v) {
+        int n=v.size(),res=0;
+        vector<int> prevmin(n,-1);
+        stack<int> st;
+        for(int i=0;i<n;i++){
+            while(st.size() && v[st.top()]>=v[i]) st.pop();
+            if(st.size()) prevmin[i]=st.top();
+            st.push(i);
+        }
+        st=stack<int>();
+        for(int i=n-1,nxtmin=n;i>=0;nxtmin=n,i--){
+            while(st.size() && v[st.top()]>=v[i]) st.pop();
+            if(st.size()) nxtmin=st.top();
+            st.push(i);
+            int x=min(nxtmin-prevmin[i]-1,v[i]);
+            res=max(res,x*x);
+        }
+        return res;
+    }
+public:
+    int maximalSquare(vector<vector<char>>& grid) {
+        int res=0,n=grid.size(),m=grid[0].size();
+        vector<vector<int>> v(n,vector<int>(m));
+        for(int j=0;j<m;j++){
+            int nxt=n;
+            for(int k=n-1;k>=0;k--){
+                if(grid[k][j]=='0') nxt=k;
+                v[k][j]=nxt-k;
+            }
+        }
+        for(int k=0;k<n;k++){
+            res=max(res,rec(v[k]));
+        }
+        return res;
+    }
+};
