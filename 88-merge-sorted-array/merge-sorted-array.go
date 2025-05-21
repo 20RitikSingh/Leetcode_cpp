2f02e07728849dd3d16a465ffec45fbcf3eed0cf
Ritik Singh <92461786+20RitikSingh@users.noreply.github.com>
2025-05-21 13:32:02 +0530

    Time: 0 ms (100.00%) | Memory: 4.3 MB (7.63%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.go
+++ 88-merge-sorted-array/merge-sorted-array.go
@@ -1,23 +1,13 @@
 func merge(nums1 []int, m int, nums2 []int, n int)  {
-    i,j:=0,0
-    var tmp []int
-    for i<m && j<n{
-        if nums1[i]<nums2[j]{
-            tmp=append(tmp,nums1[i])
-            i++
+    i,j,k:=m-1,n-1,m+n-1
+    for j>=0{
+        if i>=0 && nums1[i]>nums2[j]{
+            nums1[k]=nums1[i]
+            i--
         }else{
-            tmp=append(tmp,nums2[j])
-            j++
+            nums1[k]=nums2[j]
+            j--
         }
+        k--
     }
-    if i<m {
-        for ;i<m;i++{
-            tmp=append(tmp,nums1[i])
-        }
-    }else{
-        for ;j<n;j++{
-            tmp=append(tmp,nums2[j])
-        }
-    }
-    copy(nums1,tmp)
 }
