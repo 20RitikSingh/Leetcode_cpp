da8e5f1696a70392894a9b9d4ce08eb258b000dd
Ritik Singh <92461786+20RitikSingh@users.noreply.github.com>
2025-05-24 03:58:26 +0530

    Time: 445 ms (26.73%) | Memory: 258.8 MB (23.53%) - LeetSync

--- 2213-find-all-people-with-secret/find-all-people-with-secret.cpp
+++ 2213-find-all-people-with-secret/find-all-people-with-secret.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<int> findAllPeople(int n, vector<vector<int>>& meetings, int firstPerson) {
-        vector<int> knows(n,1e9);
+        vector<int> res,knows(n,1e9);
         vector<set<pair<int,int>>> adj(n);
         set<tuple<int,int,int>> set;
 
@@ -13,6 +13,8 @@ public:
 
         knows[0]=0;
         knows[firstPerson]=0;
+        res.push_back(0);
+        res.push_back(firstPerson);
 
         while(set.size()){
             auto it=set.begin();
@@ -21,6 +23,7 @@ public:
             for(int i=0;i<2;i++){
                 if(knows[y]<=t && knows[x]>t){
                     knows[x]=t;
+                    res.push_back(x);
                     auto it=adj[x].lower_bound({t,-1});
                     while(it!=adj[x].end()){
                         set.insert({it->first,min(x,it->second),max(x,it->second)});
@@ -31,10 +34,6 @@ public:
             }
         }
 
-        vector<int> res;
-        for(int i=0;i<n;i++){
-            if(knows[i]<1e9) res.push_back(i);
-        }
         return res;
     }
 };
