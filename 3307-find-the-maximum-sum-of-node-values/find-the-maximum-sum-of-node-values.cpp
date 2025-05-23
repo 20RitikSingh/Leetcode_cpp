c4487b7008432e23b1eee10066727bddc0ccedd0
Ritik Singh <92461786+20RitikSingh@users.noreply.github.com>
2025-05-23 07:15:33 +0530

    Time: 0 ms (100.00%) | Memory: 126.7 MB (49.68%) - LeetSync

--- 3307-find-the-maximum-sum-of-node-values/find-the-maximum-sum-of-node-values.cpp
+++ 3307-find-the-maximum-sum-of-node-values/find-the-maximum-sum-of-node-values.cpp
@@ -4,15 +4,9 @@ public:
         long long res=0;
         int mx=-1e9,c=0;
         for(int i:nums){
-            if(i>(i^k)){
-                res+=i;
-                mx=max(mx,(i^k)-i);
-            }
-            else{
-                c++;
-                res+=i^k;
-                mx=max(mx,i-(i^k));
-            }
+            res+=max(i,i^k);
+            mx=max(mx,0-abs((i^k)-i));
+            c+=i<=(i^k);
         }
         if(c%2) res+=mx;
         return res;
