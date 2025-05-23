6ce11bd3ca1e49b6bc6d6101c898a14fe4abfa11
Ritik Singh <92461786+20RitikSingh@users.noreply.github.com>
2025-05-23 06:52:41 +0530

    Time: 20 ms (31.61%) | Memory: 128.8 MB (39.35%) - LeetSync

--- 3307-find-the-maximum-sum-of-node-values/find-the-maximum-sum-of-node-values.cpp
+++ 3307-find-the-maximum-sum-of-node-values/find-the-maximum-sum-of-node-values.cpp
@@ -1,16 +1,17 @@
 class Solution {
     long long dp[20000][2];
     long long rec(vector<int> &nums,int k,int idx,bool parity){
-        if(idx==nums.size()){
-            if(parity) return -1e15;
-            return 0;
-        }
-        if(dp[idx][parity]==-1) dp[idx][parity]=max(rec(nums,k,idx+1,parity)+nums[idx],rec(nums,k,idx+1,!parity)+(nums[idx]^k));
+        if(idx==nums.size()) return parity?-1e15:0;
+        if(dp[idx][parity]==0) 
+        dp[idx][parity]=max(
+            rec(nums,k,idx+1,parity)+nums[idx],
+            rec(nums,k,idx+1,!parity)+(nums[idx]^k)
+        );
         return dp[idx][parity];
     }
 public:
     long long maximumValueSum(vector<int>& nums, int k, vector<vector<int>>& edges) {
-        memset(dp,-1,sizeof(dp));
+        // memset(dp,-1,sizeof(dp));
         return rec(nums,k,0,0);
     }
 };
