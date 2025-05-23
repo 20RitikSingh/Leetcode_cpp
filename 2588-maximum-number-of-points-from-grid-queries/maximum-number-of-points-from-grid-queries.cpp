9e1ff551eb78ccd0256573f09fb1007d6f9ca5f8
Ritik Singh <92461786+20RitikSingh@users.noreply.github.com>
2025-05-23 19:37:23 +0530

    Time: 179 ms (24.47%) | Memory: 144 MB (6.00%) - LeetSync

--- 2588-maximum-number-of-points-from-grid-queries/maximum-number-of-points-from-grid-queries.cpp
+++ 2588-maximum-number-of-points-from-grid-queries/maximum-number-of-points-from-grid-queries.cpp
@@ -1,33 +1,44 @@
 class Solution {
 public:
     vector<int> maxPoints(vector<vector<int>>& grid, vector<int>& queries) {
-        vector<int> cum(1e6+1);
         int m=grid.size(),n=grid[0].size();
-        vector<bool> vis(n*m);
-        set<pair<int,int>> set;
-        set.insert({grid[0][0],0});
-        int mx=grid[0][0];
-        while(set.size()){
-            auto it=set.begin();
-            auto [k,v]=*it;
-            // cout<<k<<" "<<v/n<<","<<v%n<<endl;
-            set.erase(it);
-            mx=max(mx,k);
-            cum[mx]++;
-            vis[v]=1;
-            int i=v/n,j=v%n;
-            if(i>0 && !vis[(i-1)*n+j]) set.insert({grid[i-1][j],(i-1)*n+j});
-            if(j>0 && !vis[i*n+j-1]) set.insert({grid[i][j-1],i*n+j-1});
-            if(i<m-1 && !vis[(i+1)*n+j]) set.insert({grid[i+1][j],(i+1)*n+j});
-            if(j<n-1 && !vis[i*n+j+1]) set.insert({grid[i][j+1],i*n+j+1});
+        priority_queue<tuple<int,int,int>> q;
+        vector<vector<bool>> vis(m,vector<bool>(n));
+        vector<int> res(queries.size());
+        int dx[4]={0,0,1,-1};
+        int dy[4]={1,-1,0,0};
+        int k=0;
+
+        vector<int> v(1e6+2);
+
+        q.push({0-grid[0][0],0,0});
+        vis[0][0]=1;
+
+        while(q.size()){
+            auto [val,r,c]=q.top();
+            q.pop();
+            // cout<<0-val-1<<" ";
+            k=max(k,0-val+1);
+            v[k]++;
+
+            for(int i=0;i<4;i++){
+                int x=r+dx[i],y=c+dy[i];
+                if(x<0 || y<0 || x>=m || y>=n || vis[x][y]) continue;
+                q.push({0-grid[x][y],x,y});
+                vis[x][y]=1;
+            }
         }
+        
+        // cout<<endl;
         for(int i=1;i<=1e6;i++){
-            cum[i]+=cum[i-1];
+            v[i]+=v[i-1];
+            // cout<<v[i]<<" ";
         }
-        vector<int> res;
-        for(int i:queries){
-            res.push_back(cum[i-1]);
+        
+        for(int i=0;i<queries.size();i++){
+            res[i]=v[queries[i]];
         }
+
         return res;
     }
 };
