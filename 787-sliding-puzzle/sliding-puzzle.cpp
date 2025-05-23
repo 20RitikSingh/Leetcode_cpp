ee3007392e0158d9f781ff6fb292401ca39822a0
Ritik Singh <92461786+20RitikSingh@users.noreply.github.com>
2025-05-24 00:41:39 +0530

    Time: 3 ms (94.37%) | Memory: 10.4 MB (93.19%) - LeetSync

--- 787-sliding-puzzle/sliding-puzzle.cpp
+++ 787-sliding-puzzle/sliding-puzzle.cpp
@@ -1,27 +1,28 @@
+unordered_map<string,int> dp;
 class Solution {
 public:
     int slidingPuzzle(vector<vector<int>>& board) {
-        string start;
+        string start,end="123450";
         for(auto i:board) for(int j:i) start.push_back(j+'0');
-        string end="123450";
+        if(dp.count(start)) return dp[start];
+        
         unordered_set<string> vis;
         queue<string> q;
+        int steps=0;
+
         q.push(start);
         vis.insert(start);
-        int steps=0;
+        
         while(q.size()){
             int sz=q.size();
-            
             for(int i=0;i<sz;i++){
                 string curr=q.front();
                 q.pop();
 
-                if(curr==end) return steps;
+                if(curr==end){dp[start]=steps; return steps;}
 
                 int pos=0;
                 for(int i=0;i<6;i++) if(curr[i]=='0'){ pos=i; break;}
-                // cout<<pos<<endl;
-                // cout<<curr<<" ";
 
                 string s=curr;
                 swap(s[pos],s[(3+pos)%6]);
@@ -29,7 +30,6 @@ public:
                     vis.insert(s);
                     q.push(s);
                 }
-                // cout<<s<<" ";
                 
                 s=curr;
                 if(pos!=0 && pos!=3){
@@ -39,7 +39,6 @@ public:
                         q.push(s);
                     }
                 }
-                // cout<<s<<" ";
 
                 s=curr;
                 if(pos!=2 && pos!=5){
@@ -49,11 +48,10 @@ public:
                         q.push(s);
                     }
                 }
-                // cout<<s<<" ";
-                // cout<<endl;
             }
             steps++;
         }
+        dp[start]=-1;
         return -1;
     }
 };
