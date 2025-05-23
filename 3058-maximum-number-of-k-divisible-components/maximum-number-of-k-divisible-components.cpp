c63434686ee2408a527fbc691b20079c2bdda927
Ritik Singh <92461786+20RitikSingh@users.noreply.github.com>
2025-05-23 21:46:26 +0530

    Time: 150 ms (37.38%) | Memory: 185.5 MB (23.74%) - LeetSync

--- 3058-maximum-number-of-k-divisible-components/maximum-number-of-k-divisible-components.cpp
+++ 3058-maximum-number-of-k-divisible-components/maximum-number-of-k-divisible-components.cpp
@@ -1,17 +1,13 @@
 class Solution {
     int res=0;
-    // int dp[30000][30000];
-    
     int dfs(vector<vector<int>> &adj,vector<int> &values,int k,int curr,int prev){
         long long sum=0;
-        // if(dp[curr][prev]!=-1) return dp[curr][prev];
         for(int i:adj[curr]){
             if(i==prev) continue;
             int x=dfs(adj,values,k,i,curr);
             if(x%k==0) res++;
             else sum+=x;
         }
-        // dp[curr][prev]=sum+values[curr];
         return (sum+values[curr])%k;
     }
 public:
@@ -21,16 +17,8 @@ public:
             adj[e[0]].push_back(e[1]);
             adj[e[1]].push_back(e[0]);
         }
-
-        // memset(dp,-1,sizeof(dp));
-
-        int ans=0;
-        for(int i=0;i<1;i++){
-            res=0;
-            int x=dfs(adj,values,k,i,i);
-            if(x%k==0) res++;
-            ans=max(ans,res);
-        }
-        return ans;
+        int x=dfs(adj,values,k,0,0);
+        if(x%k==0) res++;
+        return res;
     }
 };
