dbe4ab48b29de09b04003c77f6c30882c218b3ed
Ritik Singh <92461786+20RitikSingh@users.noreply.github.com>
2025-05-23 07:42:01 +0530

    Time: 0 ms (100.00%) | Memory: 126.7 MB (49.68%) - LeetSync

--- 3307-find-the-maximum-sum-of-node-values/find-the-maximum-sum-of-node-values.cpp
+++ 3307-find-the-maximum-sum-of-node-values/find-the-maximum-sum-of-node-values.cpp
@@ -1,16 +1,13 @@
 struct Solution{
     long long maximumValueSum(
-        vector<int>& n, int k, vector<vector<int>>& edges,
-        long res=0,int mi=1e9,bool c=0
-        ){
-            for(
-                int i=0;
-                i<n.size();
-                res+=max(n[i],n[i]^k),
-                mi=min(mi,abs((n[i]^k)-n[i])),
-                c=(c+(n[i]<=(n[i]^k)))%2,
-                i++
-            );
+        vector<int>& nums, int k, vector<vector<int>>&,
+        long long res=0,int mi=1e9,bool c=0,int i=0
+    ){
+        for(;i<nums.size();
+            res+=max(nums[i],nums[i]^k),
+            mi=min(mi,abs((nums[i]^k)-nums[i])),
+            c=(c+(nums[i]<=(nums[i++]^k)))%2
+        );
         return res-c*mi;
     }
 };
