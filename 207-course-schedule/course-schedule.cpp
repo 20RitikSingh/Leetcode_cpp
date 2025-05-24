dca84979f691fbd67747626d7390767384d2d398
Ritik Singh <92461786+20RitikSingh@users.noreply.github.com>
2025-05-25 04:52:08 +0530

    Time: 7 ms (51.39%) | Memory: 19 MB (94.57%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,26 +1,23 @@
 class Solution {
-    vector<vector<int>> adj;
+    
 public:
     bool canFinish(int n, vector<vector<int>>& edges) {
-        adj.resize(n);
+        vector<vector<int>> adj(n);
         vector<int> ind(n);
         queue<int> q;
-        for(auto e:edges){
+        for(auto &e:edges){
             ind[e[0]]++;
             adj[e[1]].push_back(e[0]);
         }
-        for(int i=0;i<n;i++){
-            if(!ind[i]) q.push(i);
-        }
+        for(int i=0;i<n;i++) if(!ind[i]) q.push(i);
         int p=0;
         while(q.size()){
-            int cur=q.front();
             p++;
-            q.pop();
-            for(int i:adj[cur]){
+            for(int i:adj[q.front()]){
                 ind[i]--;
                 if(ind[i]==0) q.push(i);
             }
+            q.pop();
         }
         return p==n;
     }
