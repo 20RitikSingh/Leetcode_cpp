e4f90a41ff22d03d6e74322fa2a25fedd2a4130a
Ritik Singh <92461786+20RitikSingh@users.noreply.github.com>
2025-05-24 06:02:23 +0530

    Time: 119 ms (21.31%) | Memory: 153.5 MB (18.04%) - LeetSync

---
+++ 2176-parallel-courses-iii/parallel-courses-iii.cpp
@@ -0,0 +1,27 @@
+class Solution {
+    vector<vector<int>> adj;
+    vector<int> vis;
+    int dp[50000];
+    int rec(vector<int> &time,int curr){
+        if(dp[curr]) return dp[curr];
+        int res=0;
+        for(int i:adj[curr]){
+            res=max(res,rec(time,i));
+        }
+        dp[curr]=res+time[curr];
+        return res+time[curr];
+    }
+public:
+    int minimumTime(int n, vector<vector<int>>& relations, vector<int>& time) {
+        adj=vector<vector<int>>(n);
+        vis.resize(n);
+        for(auto r:relations){
+            adj[r[1]-1].push_back(r[0]-1);
+        }
+        int res=0;
+        for(int i=0;i<n;i++){
+            res=max(res,rec(time,i));
+        }
+        return res;
+    }
+};
