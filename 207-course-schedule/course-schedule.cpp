300b46c1450d245005aa2d13d305f0701ff5555e
Ritik Singh <92461786+20RitikSingh@users.noreply.github.com>
2025-05-25 04:48:52 +0530

    Time: 11 ms (24.40%) | Memory: 20.1 MB (33.74%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,31 +1,27 @@
 class Solution {
     vector<vector<int>> adj;
-    vector<bool> localVis,globalVis;
-    int rec(int cur){
-        if(localVis[cur]) return -1;
-        if(globalVis[cur]) return 0;
-        globalVis[cur]=1;
-        localVis[cur]=1;
-        for(int i:adj[cur]){
-            int x=rec(i);
-            if(x<0) return -1;
-        }
-        localVis[cur]=0;
-        return 0;       
-    }
 public:
     bool canFinish(int n, vector<vector<int>>& edges) {
         adj.resize(n);
-        localVis.resize(n);
-        globalVis.resize(n);
+        vector<int> ind(n);
+        queue<int> q;
         for(auto e:edges){
+            ind[e[0]]++;
             adj[e[1]].push_back(e[0]);
         }
         for(int i=0;i<n;i++){
-            if(globalVis[i]) continue;
-            localVis=vector<bool>(n);
-            if(rec(i)<0) return 0;
+            if(!ind[i]) q.push(i);
         }
-        return 1;
+        int p=0;
+        while(q.size()){
+            int cur=q.front();
+            p++;
+            q.pop();
+            for(int i:adj[cur]){
+                ind[i]--;
+                if(ind[i]==0) q.push(i);
+            }
+        }
+        return p==n;
     }
 };
