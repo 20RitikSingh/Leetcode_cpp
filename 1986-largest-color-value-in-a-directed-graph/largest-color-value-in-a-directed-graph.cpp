0670e9eec6ef6a4a26cf7d9555d5f612d27bd158
Ritik Singh <92461786+20RitikSingh@users.noreply.github.com>
2025-05-27 00:12:48 +0530

    Time: 386 ms (50.49%) | Memory: 188.2 MB (33.89%) - LeetSync

--- 1986-largest-color-value-in-a-directed-graph/largest-color-value-in-a-directed-graph.cpp
+++ 1986-largest-color-value-in-a-directed-graph/largest-color-value-in-a-directed-graph.cpp
@@ -1,21 +1,22 @@
 class Solution {
-   
 public:
     int largestPathValue(string colors, vector<vector<int>>& edges) {
-        int n=colors.size();
+        int n=colors.size(),p=0,last=0,res=1;
         vector<vector<int>> adj(n),v(n,vector<int>(26));
         vector<int> ind(n);
         queue<int> q;
-        int p=0,last=0,res=1;
+
         for(auto e:edges){
             ind[e[0]]++;
             adj[e[1]].push_back(e[0]);
         }
+
         for(int i=0;i<n;i++){
             colors[i]-='a';
             v[i][colors[i]]++;
             if(!ind[i]) q.push(i);
         }
+
         while(q.size()){
             int cur=q.front();
             last=cur;
