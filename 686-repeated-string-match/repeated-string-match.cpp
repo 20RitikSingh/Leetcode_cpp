57eb52beb0074fc3053edd53525edd71be360b2f
Ritik Singh <92461786+20RitikSingh@users.noreply.github.com>
2025-05-29 07:36:20 +0530

    Time: 327 ms (8.30%) | Memory: 9.1 MB (96.84%) - LeetSync

--- 686-repeated-string-match/repeated-string-match.cpp
+++ 686-repeated-string-match/repeated-string-match.cpp
@@ -1,21 +1,16 @@
 class Solution {
-    bool check(string s,string t){
-        // cout<<s<<" "<<t<<endl;
-        for(int i=0;i+t.size()<=s.size();i++){
-            if(!s.compare(i,t.size(),t)) return 1;
-        }
-        return 0;
-    }
 public:
     int repeatedStringMatch(string a, string b) {
-        int c=0;
-        string s;
-        while(s.size()<b.size()){
-            s+=a;
-            c++;
+        for(int i=0;i<a.size();i++){
+            bool f=1;
+            for(int j=0;j<b.size();j++){
+                if(a[(i+j)%a.size()]!=b[j]){
+                    f=0;
+                    break;
+                }
+            }
+            if(f) return (i+b.size()+a.size()-1)/a.size();
         }
-        if(check(s,b)) return c;
-        if(check(s+a,b)) return c+1;
         return -1;
     }
 };
