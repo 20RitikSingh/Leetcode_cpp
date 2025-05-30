8f5862011e05ef45c38788aa12a9a26ad3b4d543
Ritik Singh <92461786+20RitikSingh@users.noreply.github.com>
2025-05-30 09:31:46 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (95.95%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,20 +1,15 @@
 class Solution {
+    int dfs(string &s,string &p,int i,int j){
+        if(j==p.size()) return i;
+        if(i==s.size() || s[i]!=p[j]) return -1; 
+        return dfs(s,p,i+1,j+1)>0?i:-1;
+    }
 public:
     int strStr(string s, string p) {
         for(auto &ch:p) ch-='a';
         for(auto &ch:s) ch-='a';
-        queue<int> q;
-        for(int i=0;i<s.size();i++) q.push(i);
-
-        for(int i=0;i<p.size();i++){
-            int sz=q.size();
-            for(int j=0;j<sz;j++){
-                int x=q.front();
-                q.pop();
-                if(x<s.size() && s[x]==p[i] && s.size()-x>=p.size()-i) q.push(x+1);
-            }
-            if(q.empty()) return -1;
-        }
-        return q.front()-p.size();
+        for(int i=0;i<s.size();i++) if(dfs(s,p,i,0)>=0) return i;
+        return -1;
+        
     }
 };
