fa8d94c9a2efe02606d94d4c35b93b0c3ea5efce
Ritik Singh <92461786+20RitikSingh@users.noreply.github.com>
2025-05-31 09:32:26 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (31.06%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,15 +1,33 @@
 class Solution {
-    int dfs(string &s,string &p,int i,int j){
-        if(j==p.size()) return i-p.size();
-        if(i==s.size() || s[i]!=p[j]) return -1; 
-        return dfs(s,p,i+1,j+1);
-    }
 public:
     int strStr(string s, string p) {
-        for(auto &ch:p) ch-='a';
-        for(auto &ch:s) ch-='a';
-        for(int i=0;i<s.size();i++) if(dfs(s,p,i,0)>=0) return i;
+        if(s.size()<p.size()) return -1;
+        vector<int> pi(p.size());
+        int prev=0,cur=1;
+        while(cur<p.size()){
+            if(p[prev]==p[cur]){
+                prev++;
+                pi[cur]=prev;
+                cur++;
+                continue;
+            }
+            if(prev) prev=pi[prev-1];
+            else cur++;
+        }
+        // for(int i:pi) cout<<i<<" ";
+        cur=0;
+        int i=0;
+        while(i<=s.size()){
+            // cout<<i<<" "<<cur<<endl;
+            if(cur==p.size()) return i-p.size();
+            if(s[i]==p[cur]){
+                cur++;
+                i++;
+                continue;
+            }
+            if(cur) cur=pi[cur-1];
+            else i++;
+        }
         return -1;
-        
     }
 };
