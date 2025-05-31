8d6ccd960d9179f4f64547cfeda84e477eea2bc1
Ritik Singh <92461786+20RitikSingh@users.noreply.github.com>
2025-05-31 10:55:43 +0530

    Time: 28 ms (5.21%) | Memory: 11.9 MB (97.25%) - LeetSync

--- 214-shortest-palindrome/shortest-palindrome.cpp
+++ 214-shortest-palindrome/shortest-palindrome.cpp
@@ -1,5 +1,5 @@
 class Solution {
-    int d=26,mod=1e9+7;
+    int d='z'+1,mod=1e9+7;
     int pow(int x,int y){
         if(!y) return 1;
         int res=pow(x,y/2);
@@ -7,9 +7,8 @@ class Solution {
         if(y%2) return (1LL*res*x)%mod;
         return res;
     }
-    int longestPrefixPalindrome(string s){
+    int longestPrefixPalindrome(string &s){
         int h1=0,h2=0,n=s.size(),res=0;
-        for(auto &ch:s) ch-='a';
         for(int i=0;i<n;i++){
             h1=(1LL*h1*d+s[i])%mod;
             h2=(h2+1LL*pow(d,i)*s[i])%mod;
