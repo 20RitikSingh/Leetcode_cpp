d199532b5e166da96dca9076546c973849789248
Ritik Singh <92461786+20RitikSingh@users.noreply.github.com>
2025-06-01 11:12:41 +0530

    Time: 25 ms (52.35%) | Memory: 8.9 MB (84.71%) - LeetSync

--- 3201-distribute-candies-among-children-ii/distribute-candies-among-children-ii.cpp
+++ 3201-distribute-candies-among-children-ii/distribute-candies-among-children-ii.cpp
@@ -2,9 +2,8 @@ class Solution {
 public:
     long long distributeCandies(int n, int limit) {
         long long res=0;
-        for(int i=min(n,limit);i>=max(0LL,n-2LL*limit);i--){
-            res+=max(0,min(n-i,limit)-max(0,n-i-limit)+1);
-        }
+        for(int i=min(n,limit);i>=max(0LL,n-2LL*limit);i--)
+            res+=min(n-i,limit)-max(0,n-i-limit)+1;
         return res;
     }
 };
