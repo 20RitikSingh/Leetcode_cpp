df15b7e15d3dbf3279c0a4f7d5f490374c798fe2
Ritik Singh <92461786+20RitikSingh@users.noreply.github.com>
2025-06-03 08:45:13 +0530

    Time: 61 ms (6.35%) | Memory: 129.3 MB (5.36%) - LeetSync

--- 2690-house-robber-iv/house-robber-iv.cpp
+++ 2690-house-robber-iv/house-robber-iv.cpp
@@ -1,16 +1,13 @@
 class Solution {
     bool check(vector<int> &nums,int k,int mi){
         int n=nums.size();
-        if(n<3){
-            if(n==1 && k==1 ) return nums[0]<=mi;
-            if(n==2 && k==1 ) return (nums[0]<=mi || nums[1]<=mi);
-            return 0;
-        }
         vector<int> v(n);
         v[0]=(nums[0]<=mi);
-        v[1]=max(v[0],int(nums[1]<=mi));
-        for(int i=2;i<n;i++){
-            v[i]=max(v[i-2]+(nums[i]<=mi),v[i-1]);
+        if(v[0]==k) return 1;
+        for(int i=1;i<n;i++){
+            v[i]=nums[i]<=mi;
+            if(i>1) v[i]+=v[i-2];
+            v[i]=max(v[i],v[i-1]);
             if(v[i]>=k) return 1;
         }
         return 0;
