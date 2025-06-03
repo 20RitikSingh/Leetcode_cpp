195b6a58683661b7541aff279665b3b030de2c34
Ritik Singh <92461786+20RitikSingh@users.noreply.github.com>
2025-06-03 09:59:56 +0530

    Time: 64 ms (59.68%) | Memory: 348.4 MB (22.60%) - LeetSync

--- 3643-zero-array-transformation-ii/zero-array-transformation-ii.cpp
+++ 3643-zero-array-transformation-ii/zero-array-transformation-ii.cpp
@@ -1,29 +1,23 @@
 class Solution {
-    bool check(vector<int> &nums, vector<vector<int>>& q,int k){
-        vector<int> u(nums.size()+1);
-        for(int i=0;i<k;i++){
-            int l=q[i][0];
-            int r=q[i][1];
-            int v=q[i][2];
-            u[l]-=v;
-            u[r+1]+=v;
-        }
-        int sum=0;
-        for(int i=0;i<nums.size();i++){
-            if(i>0) u[i]+=u[i-1];
-            if(nums[i]+u[i]>0) return 0;
-        }
+    bool check(vector<int>& nums, vector<vector<int>>& queries,int k) {
+        int n=nums.size();
+        vector<int> v(n+1);
+        for(int i=0;i<k;i++) 
+            v[queries[i][0]]+=queries[i][2],v[queries[i][1]+1]-=queries[i][2];
+        if(v[0]<nums[0]) return 0;
+        for(int i=1;i<n;i++){ v[i]+=v[i-1]; if(v[i]<nums[i]) return 0;}
         return 1;
+
     }
 public:
     int minZeroArray(vector<int>& nums, vector<vector<int>>& queries) {
         int low=0,high=queries.size();
-        if(!check(nums,queries,high)) return -1;
         while(low<high){
             int mid=low+(high-low)/2;
             if(check(nums,queries,mid)) high=mid;
             else low=mid+1;
         }
-        return low;
+        if(check(nums,queries,low)) return low;
+        return -1;
     }
 };
