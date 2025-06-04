789752b982196df78cee65bce3b7d95d75b30aa5
Ritik Singh <92461786+20RitikSingh@users.noreply.github.com>
2025-06-04 14:21:00 +0530

    Time: 2 ms (75.55%) | Memory: 10.3 MB (45.39%) - LeetSync

--- 51-n-queens/n-queens.cpp
+++ 51-n-queens/n-queens.cpp
@@ -3,20 +3,12 @@ class Solution {
     void rec(vector<string> &v,int idx){
         int n=v.size();
         if(idx==n) res.push_back(v);
-        else
-        for(int i=0;i<n;i++){
-            bool f=1;
-            for(int j=0;j<=idx;j++){
-                if(v[j][i]=='Q' || (i>=j && v[idx-j][i-j]=='Q') || (i+j<n && v[idx-j][i+j]=='Q')){
-                    f=0;
-                    break;
-                }
-            }
-            if(f){
-                v[idx][i]='Q';
-                rec(v,idx+1);
-                v[idx][i]='.';
-            }
+        else for(int i=0;i<n;i++){
+            for(int j=0;j<=idx;j++) if(v[j][i]=='Q' || (i>=j && v[idx-j][i-j]=='Q') || (i+j<n && v[idx-j][i+j]=='Q')) goto skip;
+            v[idx][i]='Q';
+            rec(v,idx+1);
+            v[idx][i]='.';
+            skip:;
         }
     }
 public:
