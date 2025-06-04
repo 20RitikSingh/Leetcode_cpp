68bbeb90044b54811ad498086a076c5adedcdb91
Ritik Singh <92461786+20RitikSingh@users.noreply.github.com>
2025-06-04 12:22:30 +0530

    Time: 79 ms (23.88%) | Memory: 185.7 MB (6.72%) - LeetSync

--- 3683-find-the-lexicographically-largest-string-from-the-box-i/find-the-lexicographically-largest-string-from-the-box-i.cpp
+++ 3683-find-the-lexicographically-largest-string-from-the-box-i/find-the-lexicographically-largest-string-from-the-box-i.cpp
@@ -4,9 +4,8 @@ public:
         string v;
         int k=word.size()-numFriends+1;
         if(k==word.size()) return word;
-        for(int i=0;i<word.size();i++){
-            if(v<word.substr(i,min(k,(int)word.size()-i))) v=word.substr(i,min(k,(int)word.size()-i));
-        }
+        for(int i=0;i<word.size();i++)
+        if(v<word.substr(i,min(k,(int)word.size()-i))) v=word.substr(i,min(k,(int)word.size()-i));
         return v;
     }
 };
