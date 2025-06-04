b46a292e544339ba375e6ce211dd841e5f42ec87
Ritik Singh <92461786+20RitikSingh@users.noreply.github.com>
2025-06-04 12:19:17 +0530

    Time: 79 ms (23.88%) | Memory: 185.7 MB (6.72%) - LeetSync

--- 3683-find-the-lexicographically-largest-string-from-the-box-i/find-the-lexicographically-largest-string-from-the-box-i.cpp
+++ 3683-find-the-lexicographically-largest-string-from-the-box-i/find-the-lexicographically-largest-string-from-the-box-i.cpp
@@ -1,13 +1,12 @@
 class Solution {
 public:
     string answerString(string word, int numFriends) {
-        vector<string> v;
+        string v;
         int k=word.size()-numFriends+1;
         if(k==word.size()) return word;
         for(int i=0;i<word.size();i++){
-            v.push_back(word.substr(i,min(k,(int)word.size()-i)));
+            if(v<word.substr(i,min(k,(int)word.size()-i))) v=word.substr(i,min(k,(int)word.size()-i));
         }
-        sort(v.begin(),v.end());
-        return v.back();
+        return v;
     }
 };
