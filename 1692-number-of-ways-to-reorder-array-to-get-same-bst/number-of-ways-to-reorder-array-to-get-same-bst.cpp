f419afb87d744cd4152a2242585d663f54c9eaa4
Ritik Singh <92461786+20RitikSingh@users.noreply.github.com>
2025-06-06 18:28:11 +0530

    Time: 52 ms (80.40%) | Memory: 50.3 MB (74.23%) - LeetSync

--- 1692-number-of-ways-to-reorder-array-to-get-same-bst/number-of-ways-to-reorder-array-to-get-same-bst.cpp
+++ 1692-number-of-ways-to-reorder-array-to-get-same-bst/number-of-ways-to-reorder-array-to-get-same-bst.cpp
@@ -1,9 +1,8 @@
-class Solution {
 int mod=1e9+7;
 vector<vector<long long>> table;
-public:
-    int numOfWays(vector<int>& nums) {
-        int n=nums.size();
+struct init{
+    init(){
+        int n=1000;
         table.resize(n + 1);
         for(int i = 0; i < n + 1; ++i){
             table[i] = vector<long long>(i + 1, 1);
@@ -11,7 +10,13 @@ public:
                 table[i][j] = (table[i-1][j-1] + table[i-1][j]) % mod;
             }
         }
-
+    }
+};
+init init;
+class Solution {
+public:
+    int numOfWays(vector<int>& nums) {
+        int n=nums.size();
         queue<vector<int>> q;
         q.push(nums);
         int res=1;
@@ -25,7 +30,7 @@ public:
             }
             if(x.size()) q.push(x);
             if(y.size()) q.push(y);
-            res=(res*table[x.size()+y.size()][x.size()])%mod;
+            res=(1LL*res*table[x.size()+y.size()][x.size()])%mod;
         }
         return res-1;
     }
