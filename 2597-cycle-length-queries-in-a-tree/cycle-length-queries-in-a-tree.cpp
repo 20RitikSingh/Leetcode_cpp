cc07795ae369d024c0a41c14f1a8fa7b8785f45a
Ritik Singh <92461786+20RitikSingh@users.noreply.github.com>
2025-06-07 08:00:43 +0530

    Time: 35 ms (42.05%) | Memory: 78.4 MB (24.10%) - LeetSync

--- 2597-cycle-length-queries-in-a-tree/cycle-length-queries-in-a-tree.cpp
+++ 2597-cycle-length-queries-in-a-tree/cycle-length-queries-in-a-tree.cpp
@@ -1,22 +1,13 @@
 class Solution {
-    int depth(int x){
-        return logb(x);
-    }
     int lca(int x,int y){
-        // cout<<"lca: "<<x<<" "<<y<<endl;
         bitset<32> p(x),q(y);
-        // cout<<"x: "<<p<<endl;
-        // cout<<"y: "<<q<<endl;
         int i=31,j=31;
         while(!p[i]) i--;
         while(!q[j]) j--;
-        // cout<<i<<" "<<j<<endl;
         int c=i+j+2;
-        // cout<<c<<endl;
         while(i>=0 && j>=0 && p[i--]==q[j--]){
             c-=2;
         }
-        // cout<<c<<endl;
         return c+1;
     }
 public:
