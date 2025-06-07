43dcafc2e7192de44b5d96df05eac2d04e15a3d8
Ritik Singh <92461786+20RitikSingh@users.noreply.github.com>
2025-06-07 08:04:12 +0530

    Time: 40 ms (35.90%) | Memory: 78.3 MB (34.36%) - LeetSync

--- 2597-cycle-length-queries-in-a-tree/cycle-length-queries-in-a-tree.cpp
+++ 2597-cycle-length-queries-in-a-tree/cycle-length-queries-in-a-tree.cpp
@@ -1,15 +1,22 @@
 class Solution {
-    int lca(int x,int y){
-        bitset<32> p(x),q(y);
-        int i=31,j=31;
-        while(!p[i]) i--;
-        while(!q[j]) j--;
-        int c=i+j+2;
-        while(i>=0 && j>=0 && p[i--]==q[j--]){
-            c-=2;
-        }
-        return c+1;
+    int msb_pos(int n) {
+    if (n == 0) return -1;
+    int pos = 0;
+    while (n >>= 1) pos++;
+    return pos;
+}
+
+int lca(int x, int y) {
+    int i = msb_pos(x);
+    int j = msb_pos(y);
+    int c = i + j + 2;
+    while (i >= 0 && j >= 0 && ((x >> i) & 1) == ((y >> j) & 1)) {
+        c -= 2;
+        i--; j--;
     }
+    return c + 1;
+}
+
 public:
     vector<int> cycleLengthQueries(int n, vector<vector<int>>& queries) {
         vector<int> res;
