4d9d2cf5bb66b738426353d663021dd868ff218e
Ritik Singh <92461786+20RitikSingh@users.noreply.github.com>
2025-06-07 08:08:03 +0530

    Time: 8 ms (83.59%) | Memory: 70.5 MB (72.82%) - LeetSync

--- 2597-cycle-length-queries-in-a-tree/cycle-length-queries-in-a-tree.cpp
+++ 2597-cycle-length-queries-in-a-tree/cycle-length-queries-in-a-tree.cpp
@@ -1,18 +1,15 @@
 class Solution {
-    int lca(int x, int y) {
-        int count = 0;
-        while (x != y) {
-            if (x > y) x /= 2;
-            else y /= 2;
-            count++;
-        }
-        return count + 1;
-    }
 public:
     vector<int> cycleLengthQueries(int n, vector<vector<int>>& queries) {
         vector<int> res;
         for (auto& q : queries) {
-            res.push_back(lca(q[0], q[1]));
+            int count = 0;
+            while (q[0] != q[1]) {
+                if (q[0] > q[1]) q[0] /= 2;
+                else q[1] /= 2;
+                count++;
+            }
+            res.push_back(count+1);
         }
         return res;
     }
