4c0ae8e61837845a21dd97208766f1c457099f13
Ritik Singh <92461786+20RitikSingh@users.noreply.github.com>
2025-06-08 13:12:54 +0530

    Time: 638 ms (5.00%) | Memory: 14.7 MB (76.35%) - LeetSync

--- 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
@@ -4,12 +4,11 @@ class Solution {
         int n=nums.size();
         if(!k || idx==n) return 0;
         if(dp[k][idx]!=-1) return dp[k][idx];
-        int res=rec(nums,k,idx+1);
+        dp[k][idx]=rec(nums,k,idx+1);
         for(int i=idx+1;i<n;i++){
-            res=max(res,rec(nums,k-1,i+1)+nums[i]-nums[idx]);
+            dp[k][idx]=max(dp[k][idx],rec(nums,k-1,i+1)+nums[i]-nums[idx]);
         }
-        dp[k][idx]=res;
-        return res;
+        return dp[k][idx];
     }
 public:
     int maxProfit(int k, vector<int>& nums) {
