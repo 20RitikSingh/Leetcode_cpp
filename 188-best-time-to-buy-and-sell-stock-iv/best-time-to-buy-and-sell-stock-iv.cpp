9378bb3ee00ad62f880f66ac502654d690481b8a
Ritik Singh <92461786+20RitikSingh@users.noreply.github.com>
2025-06-08 13:37:36 +0530

    Time: 14 ms (35.57%) | Memory: 16 MB (67.04%) - LeetSync

--- 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,18 +1,16 @@
 class Solution {
-    int dp[101][1001];
-    int rec(vector<int> &nums,int k,int idx){
-        int n=nums.size();
-        if(!k || idx==n) return 0;
-        if(dp[k][idx]!=-1) return dp[k][idx];
-        dp[k][idx]=rec(nums,k,idx+1);
-        for(int i=idx+1;i<n;i++){
-            dp[k][idx]=max(dp[k][idx],rec(nums,k-1,i+1)+nums[i]-nums[idx]);
-        }
-        return dp[k][idx];
+    int dp[201][1001][2];
+    int rec(vector<int> &nums,int k,int idx,bool f){
+        if(k==0 || idx==nums.size()) return k%2?-1e9:0;
+        if(dp[k][idx][f]!=-1) return dp[k][idx][f];
+        dp[k][idx][f]=rec(nums,k,idx+1,f);
+        if(f) dp[k][idx][f]=max(dp[k][idx][f],rec(nums,k-1,idx+1,0)+nums[idx]);
+        else dp[k][idx][f]=max(dp[k][idx][f],rec(nums,k-1,idx+1,1)-nums[idx]);
+        return dp[k][idx][f];
     }
 public:
     int maxProfit(int k, vector<int>& nums) {
         memset(dp,-1,sizeof(dp));
-        return rec(nums,k,0);
+        return rec(nums,2*k,0,0);
     }
 };
