91bb5472c2720993884388871f5934ff4c69772d
Ritik Singh <92461786+20RitikSingh@users.noreply.github.com>
2025-06-09 18:37:34 +0530

    Time: 3 ms (89.35%) | Memory: 21.6 MB (15.15%) - LeetSync

---
+++ 337-house-robber-iii/house-robber-iii.py
@@ -0,0 +1,16 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0,.leftNone,.rightNone):
+#         self.val = val
+#         self.left=.left#         self.right=.right
+class Solution:
+    @cache
+    def rob(self, root: Optional[TreeNode]) -> int:
+        if root == None:
+            return 0
+        x=root.val
+        if root.left != None :
+            x+=self.rob(root.left.left)+self.rob(root.left.right)
+        if root.right != None :
+            x+=self.rob(root.right.left)+self.rob(root.right.right)
+        return max(x,self.rob(root.left)+self.rob(root.right))
