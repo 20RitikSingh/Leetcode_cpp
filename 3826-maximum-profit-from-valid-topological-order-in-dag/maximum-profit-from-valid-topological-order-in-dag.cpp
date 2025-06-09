3d0f21332ab9ee317bb689dc3af7dc580709f208
Ritik Singh <92461786+20RitikSingh@users.noreply.github.com>
2025-06-09 07:45:32 +0530

    Time: 1370 ms (35.52%) | Memory: 64.1 MB (93.30%) - LeetSync

---
+++ 3826-maximum-profit-from-valid-topological-order-in-dag/maximum-profit-from-valid-topological-order-in-dag.cpp
@@ -0,0 +1,21 @@
+class Solution {
+    int dp[1<<22],req[22];
+    int rec(int msk,vector<int> &s){
+        if(dp[msk]!=-1) return dp[msk];
+        int res=0,idx=__builtin_popcount(msk) + 1;
+        for(int i=0;i<s.size();i++){
+            if(msk&(1<<i)) continue;
+            if((msk&req[i])<req[i]) continue;
+            res=max(res,rec(msk|(1<<i),s)+idx*s[i]);
+        }
+        dp[msk]=res;
+        return res;
+    }
+public:
+    int maxProfit(int n, vector<vector<int>>& edges, vector<int>& s) {
+        memset(dp,-1,sizeof(dp));
+        for(auto e:edges) req[e[1]]|=1<<e[0];
+        return rec(0,s);
+        return 0;
+    }
+};
