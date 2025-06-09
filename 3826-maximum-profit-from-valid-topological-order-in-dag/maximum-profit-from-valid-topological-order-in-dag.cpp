b4ffbc91fb662412f4db3e413bc407f1113aa482
Ritik Singh <92461786+20RitikSingh@users.noreply.github.com>
2025-06-09 07:50:05 +0530

    Time: 836 ms (62.45%) | Memory: 185.5 MB (77.76%) - LeetSync

--- 3826-maximum-profit-from-valid-topological-order-in-dag/maximum-profit-from-valid-topological-order-in-dag.cpp
+++ 3826-maximum-profit-from-valid-topological-order-in-dag/maximum-profit-from-valid-topological-order-in-dag.cpp
@@ -1,11 +1,10 @@
-int dp[1<<22],req[22];
 class Solution {
+    vector<int> dp,req;
     int rec(int msk,vector<int> &s){
         if(dp[msk]!=-1) return dp[msk];
         int res=0,idx=__builtin_popcount(msk) + 1;
         for(int i=0;i<s.size();i++){
-            if(msk&(1<<i)) continue;
-            if((msk&req[i])<req[i]) continue;
+            if(msk&(1<<i) || (msk&req[i])<req[i]) continue;
             res=max(res,rec(msk|(1<<i),s)+idx*s[i]);
         }
         dp[msk]=res;
@@ -13,8 +12,8 @@ class Solution {
     }
 public:
     int maxProfit(int n, vector<vector<int>>& edges, vector<int>& s) {
-        memset(dp,-1,sizeof(dp));
-        memset(req,0,sizeof(req));
+        req.resize(n);
+        dp.resize(1<<n,-1);
         for(auto e:edges) req[e[1]]|=1<<e[0];
         return rec(0,s);
         return 0;
