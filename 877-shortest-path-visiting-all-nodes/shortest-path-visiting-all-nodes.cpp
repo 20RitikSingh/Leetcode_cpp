bc4afc01803462e447073f787d97ee6859a0a126
Ritik Singh <92461786+20RitikSingh@users.noreply.github.com>
2025-06-12 15:01:08 +0530

    Time: 556 ms (5.04%) | Memory: 68.7 MB (5.35%) - LeetSync

--- 877-shortest-path-visiting-all-nodes/shortest-path-visiting-all-nodes.cpp
+++ 877-shortest-path-visiting-all-nodes/shortest-path-visiting-all-nodes.cpp
@@ -1,15 +1,19 @@
 class Solution {
+    int n;
+    int comp(int msk){
+        return ((1<<n)-1)^msk;
+    }
 public:
     int shortestPathLength(vector<vector<int>>& adj) {
-        int n=adj.size();
+        n=adj.size();
         set<tuple<int,int,int>> set;
-        for(int i=0;i<n;i++) set.insert({0,i,1<<i});
+        for(int i=0;i<n;i++) set.insert({0,i,comp(1<<i)});
         while(set.size()){
             auto [cst,cur,msk]=*set.begin();
             set.erase(*set.begin());
-            if(msk+1==1<<n) return cst;
+            if(msk==0) return cst;
             for(int i:adj[cur]){
-                set.insert({cst+1,i,msk|(1<<i)});
+                set.insert({cst+1,i,comp(comp(msk)|(1<<i))});
             }
         }
         return 0;
