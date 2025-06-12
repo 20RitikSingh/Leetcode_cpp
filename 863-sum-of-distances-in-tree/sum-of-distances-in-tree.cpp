887e7ae4bd63b7b5e31d734a9dee7bb57b2bc132
Ritik Singh <92461786+20RitikSingh@users.noreply.github.com>
2025-06-12 14:41:52 +0530

    Time: 123 ms (31.82%) | Memory: 105.2 MB (40.43%) - LeetSync

--- 863-sum-of-distances-in-tree/sum-of-distances-in-tree.cpp
+++ 863-sum-of-distances-in-tree/sum-of-distances-in-tree.cpp
@@ -1,11 +1,12 @@
 class Solution {
     vector<int> res,v;
     vector<vector<int>> adj;
-    int dfs(int cur,int prev){
+    int dfs(int cur,int prev,int d){
         int ans=1;
+        res[0]+=d;
         for(int i:adj[cur]){
             if(i==prev) continue;
-            v[i]=dfs(i,cur);
+            v[i]=dfs(i,cur,d+1);
             ans+=v[i];
         }
         return ans;
@@ -18,13 +19,6 @@ class Solution {
             dfs1(i,cur);
         }
     }
-    void dfs2(int cur,int prev,int d){
-        res[0]+=d;
-        for(int i:adj[cur]){
-            if(i==prev) continue;
-            dfs2(i,cur,d+1);
-        }
-    }
 public:
     vector<int> sumOfDistancesInTree(int n, vector<vector<int>>& edges) {
         res.resize(n);
@@ -34,8 +28,7 @@ public:
             adj[e[0]].push_back(e[1]);
             adj[e[1]].push_back(e[0]);
         }
-        dfs2(0,-1,0); 
-        dfs(0,-1);
+        dfs(0,-1,0);
         dfs1(0,-1);
         return res;
     }
