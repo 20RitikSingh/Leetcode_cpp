63dbddc60562c4f952008b0edf8a78ef3994591c
Ritik Singh <92461786+20RitikSingh@users.noreply.github.com>
2025-06-14 12:33:01 +0530

    Time: 240 ms (36.42%) | Memory: 10.3 MB (98.15%) - LeetSync

--- 2114-minimum-number-of-work-sessions-to-finish-the-tasks/minimum-number-of-work-sessions-to-finish-the-tasks.cpp
+++ 2114-minimum-number-of-work-sessions-to-finish-the-tasks/minimum-number-of-work-sessions-to-finish-the-tasks.cpp
@@ -1,26 +1,31 @@
 class Solution {
+    int dp2[1<<14];
+    int check(vector<int> &nums,int k,int msk){
+        int sum=0;
+        if(dp2[msk]) return dp2[msk]-1;
+        for(int i=0;i<nums.size();i++){
+            if(msk&(1<<i)) sum+=nums[i];
+        }
+        dp2[msk]=1+(sum<=k);
+        return sum<=k;
+    }
+    int dp[1<<14];
+    int rec(vector<int> &nums,int k,int msk){
+        int n=nums.size(),res=1e9;
+        if(msk+1==1<<n) return 0;
+        if(dp[msk]) return dp[msk];
+        int subset = ((1 << n) - 1) ^ msk;
+        for (int sub = subset; sub>0; sub = (sub - 1) & subset) {
+            if (check(nums, k, sub)) {
+                res = min(res, rec(nums, k, msk | sub) + 1);
+            }
+        }
+        dp[msk]=res;
+        return res;
+    }
 public:
     int minSessions(vector<int>& tasks, int sessionTime) {
-        int n = tasks.size();
-        sort(tasks.rbegin(), tasks.rend());           // 5⃣
-        int FULL = (1 << n) - 1;
-
-        /* 2⃣ pre‑compute subset sums */
-        vector<int> sum(1 << n, 0);
-        for (int m = 1; m <= FULL; ++m) {
-            int lsb = m & -m;
-            int idx = __builtin_ctz(lsb);
-            sum[m] = sum[m ^ lsb] + tasks[idx];
-        }
-
-        /* 3⃣ bottom‑up bitmask DP */
-        vector<uint8_t> dp(1 << n, n);                // 0..14 fits in a byte
-        dp[0] = 0;
-        for (int m = 1; m <= FULL; ++m) {
-            for (int sub = m; sub; sub = (sub - 1) & m)
-                if (sum[sub] <= sessionTime)
-                    dp[m] = min<uint8_t>(dp[m], dp[m ^ sub] + 1);
-        }
-        return dp[FULL];
+        sort(tasks.begin(),tasks.end());
+        return rec(tasks,sessionTime,0);
     }
 };
