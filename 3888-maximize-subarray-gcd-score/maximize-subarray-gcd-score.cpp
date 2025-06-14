4d277fe7233a97d775a30d4748677f3176b84e56
Ritik Singh <92461786+20RitikSingh@users.noreply.github.com>
2025-06-14 17:00:43 +0530

    Time: 962 ms (31.86%) | Memory: 36.2 MB (94.45%) - LeetSync

--- 3888-maximize-subarray-gcd-score/maximize-subarray-gcd-score.cpp
+++ 3888-maximize-subarray-gcd-score/maximize-subarray-gcd-score.cpp
@@ -12,18 +12,16 @@ public:
         int n=nums.size();
         long long res=0;
         for(int i=0;i<n;i++){
-            vector<int> v(32);
-            int g=nums[i];
+            int g=nums[i],c=0,mi=32;
             for(int j=i;j<n;j++){
                 g=gcd(g,nums[j]);
+                int x=pow2(nums[j]);
+                if(x<mi){
+                    c=1;
+                    mi=x;
+                }else if(x==mi) c++;
                 long long ans=1LL*g*(j-i+1);
-                v[pow2(nums[j])]++;
-                for(int p=0;p<32;p++){
-                    if(v[p]){
-                        if(k>=v[p]) ans*=2;
-                        break;
-                    }
-                }
+                if(k>=c) ans*=2;
                 res=max(res,ans);
             }
         }
