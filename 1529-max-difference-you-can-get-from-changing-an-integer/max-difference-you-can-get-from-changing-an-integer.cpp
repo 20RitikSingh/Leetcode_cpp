684d14cec513b84628079f50f649f731c9927e9a
Ritik Singh <92461786+20RitikSingh@users.noreply.github.com>
2025-06-15 17:52:09 +0530

    Time: 2 ms (21.80%) | Memory: 8.6 MB (19.43%) - LeetSync

--- 1529-max-difference-you-can-get-from-changing-an-integer/max-difference-you-can-get-from-changing-an-integer.cpp
+++ 1529-max-difference-you-can-get-from-changing-an-integer/max-difference-you-can-get-from-changing-an-integer.cpp
@@ -1,27 +1,27 @@
 class Solution {
-    int solve(int num,int i,int j){
+public:
+    int maxDiff(int num) {
+        int mx=0,mi=0,t=0,u=0,f=0;
         stack<int> st;
         while(num){
             st.push(num%10);
+            if(num%10<9) t=num%10;
+            if(num%10>1) u=num%10;
             num/=10;
         }
-        if(st.top()==i && j==0) return -1;
+        if(u==st.top()) f=1;
         while(st.size()){
-            if(st.top()==i) num=num*10+j;
-            else num=num*10+st.top();
+            if(st.top()==t){
+                mx=mx*10+9;
+            }else{
+                mx=mx*10+st.top();
+            }
+            if(st.top()==u){
+                mi=mi*10+f;
+            }else{
+                mi=mi*10+st.top();
+            }
             st.pop();
-        }
-        return num;
-    }
-public:
-    int maxDiff(int num) {
-        int mx=num,mi=num;
-        for(int j=0;j<10;j++)
-        for(int i=0;i<10;i++){
-            int x=solve(num,i,j);
-            if(x<0) continue;
-            mi=min(mi,x);
-            mx=max(mx,x);
         }
         return mx-mi;
     }
