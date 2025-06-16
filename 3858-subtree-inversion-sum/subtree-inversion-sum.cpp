ec38f0b221726f5586d7c09a3d90629d0367bb75
Ritik Singh <92461786+20RitikSingh@users.noreply.github.com>
2025-06-16 16:05:01 +0530

    Time: 491 ms (76.64%) | Memory: 347.8 MB (57.83%) - LeetSync

--- 3858-subtree-inversion-sum/subtree-inversion-sum.cpp
+++ 3858-subtree-inversion-sum/subtree-inversion-sum.cpp
@@ -1,41 +1,60 @@
 class Solution {
+    using ll = long long;
+    const ll INF = 4e18;
+
     vector<vector<int>> adj;
-    vector<int> v;
-    int cooldown;
-    pair<long long,long long> dp[100001][51];
-    pair<long long,long long> dfs(int cur,int prev,int k){
-        if(dp[cur][k]!=make_pair(1e15,-1e15)) return dp[cur][k];
-        long long mi=v[cur],mx=v[cur],mi1=v[cur],mx1=v[cur];
-        pair<long long ,long long> res={mi,mx};
-        for(int i:adj[cur]){
-            if(prev==i) continue;
-            auto [x,y]=dfs(i,cur,max(0,k-1));
-            mi+=x;
-            mx+=y;
-            if(k<=0) {
-                auto [p,q]=dfs(i,cur,cooldown);
-                mi1+=p;
-                mx1+=q;
+    vector<int> values;
+    int maxCooldown;
+    vector<vector<pair<ll, ll>>> dp;
+
+    pair<ll, ll> dfs(int node, int parent, int k) {
+        if (dp[node][k].first != INF) return dp[node][k];
+
+        ll sumMin = values[node], sumMax = values[node];
+        ll altMin = values[node], altMax = values[node];
+
+        bool canInvert = (k <= 0);
+
+        for (int child : adj[node]) {
+            if (child == parent) continue;
+
+            auto [minSub, maxSub] = dfs(child, node, max(0, k - 1));
+            sumMin += minSub;
+            sumMax += maxSub;
+
+            if (canInvert) {
+                auto [invMinSub, invMaxSub] = dfs(child, node, maxCooldown);
+                altMin += invMinSub;
+                altMax += invMaxSub;
             }
         }
-        if(k<=0){
-            res.first=min(mi,-mx1);
-            res.second=max(mx,-mi1);
-        }else res={mi,mx};
-        dp[cur][k]=res;
-        return res;
+
+        if (canInvert) {
+            dp[node][k] = {
+                min(sumMin, -altMax),
+                max(sumMax, -altMin)
+            };
+        } else {
+            dp[node][k] = {sumMin, sumMax};
+        }
+
+        return dp[node][k];
     }
+
 public:
     long long subtreeInversionSum(vector<vector<int>>& edges, vector<int>& nums, int k) {
-        int n=nums.size();
-        adj.resize(n);
-        v=nums;
-        for(auto e:edges){
+        int n = nums.size();
+        adj.assign(n, {});
+        values = nums;
+        maxCooldown = k - 1;
+
+        for (auto& e : edges) {
             adj[e[0]].push_back(e[1]);
             adj[e[1]].push_back(e[0]);
         }
-        cooldown=k-1;
-        for(int i=0;i<n;i++) for(int j=0;j<k+1;j++) dp[i][j]={1e15,-1e15};
-        return dfs(0,-1,0).second;
+
+        dp.assign(n, vector<pair<ll, ll>>(k + 1, {INF, -INF}));
+
+        return dfs(0, -1, 0).second;
     }
 };
