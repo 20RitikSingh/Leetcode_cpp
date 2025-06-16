3b24c8af0bdeafc757146a43fba0d2237ca12a35
Ritik Singh <92461786+20RitikSingh@users.noreply.github.com>
2025-06-16 18:11:15 +0530

    Time: 124 ms (21.03%) | Memory: 207.3 MB (25.64%) - LeetSync

--- 3744-minimum-operations-to-make-array-elements-zero/minimum-operations-to-make-array-elements-zero.cpp
+++ 3744-minimum-operations-to-make-array-elements-zero/minimum-operations-to-make-array-elements-zero.cpp
@@ -1,18 +1,11 @@
 class Solution {
-    int count(int l,int r,int b){
-        int mi=1<<(b-1);
-        int mx=(1<<b)-1;
-        mi=max(mi,l);
-        mx=min(mx,r);
-        return max(0,mx-mi+1);
-    }
 public:
     long long minOperations(vector<vector<int>>& queries) {
         long long res=0;
         for(auto q:queries){
             long long clicks=0;
             for(int i=1;i<31;i++){
-                clicks+=1LL*(i+1)/2*count(q[0],q[1],i);
+                clicks+=1LL*(i+1)/2*max(0,min((1<<i)-1,q[1])-max(1<<(i-1),q[0])+1);
             }
             res+=(clicks+1)/2;
         }
