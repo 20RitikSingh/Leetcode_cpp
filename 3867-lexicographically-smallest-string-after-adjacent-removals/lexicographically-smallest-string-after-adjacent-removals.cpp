35c4e4dc04abc3c319ad9539df648b29c214d018
Ritik Singh <92461786+20RitikSingh@users.noreply.github.com>
2025-06-16 12:00:40 +0530

    Time: 498 ms (71.07%) | Memory: 45.2 MB (56.20%) - LeetSync

--- 3867-lexicographically-smallest-string-after-adjacent-removals/lexicographically-smallest-string-after-adjacent-removals.cpp
+++ 3867-lexicographically-smallest-string-after-adjacent-removals/lexicographically-smallest-string-after-adjacent-removals.cpp
@@ -1,6 +1,7 @@
 class Solution {
     char dp[250][250];
-    bool check(string &s,int low,int high){
+    string dp2[251];
+     bool check(string &s,int low,int high){
         if(high<low) return 1;
         if(dp[low][high]) return dp[low][high]-1;
         if(abs(s[low]-s[high])%24==1) dp[low][high]|=check(s,low+1,high-1);
@@ -8,17 +9,12 @@ class Solution {
             dp[low][high]|=check(s,low,i)&check(s,i+1,high);
         return dp[low][high]++;
     }
-    string dp2[250];
-    string rec(string &s,int i){
-        int n=s.size();
-        if(check(s,i,n-1)) return "";
-        if(dp2[i]!="") return dp2[i];
-        dp2[i]=s.substr(i,1)+rec(s,i+1);
-        for(int j=i+2;j<n;j+=2) if(check(s,i,j-1)) dp2[i]=min(dp2[i],rec(s,j));
-        return dp2[i];
-    }
 public:
-    string lexicographicallySmallestString(string s) {
-        return rec(s,0);
+    string lexicographicallySmallestString(string s,int i=0) {
+        int n=s.size();
+        if(check(s,i,n-1) || dp2[i]!="") return dp2[i];
+        dp2[i]=s.substr(i,1)+lexicographicallySmallestString(s,i+1);
+        for(int j=i+2;j<n;j+=2) if(check(s,i,j-1)) dp2[i]=min(dp2[i],lexicographicallySmallestString(s,j));
+        return dp2[i];
     }
 };
