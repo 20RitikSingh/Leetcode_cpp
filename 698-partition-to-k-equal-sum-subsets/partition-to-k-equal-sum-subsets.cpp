5bced76907b89924b73e9cb5a45033863bea000d
Ritik Singh <92461786+20RitikSingh@users.noreply.github.com>
2025-06-17 12:09:45 +0530

    Time: 406 ms (34.98%) | Memory: 62.7 MB (5.67%) - LeetSync

--- 698-partition-to-k-equal-sum-subsets/partition-to-k-equal-sum-subsets.cpp
+++ 698-partition-to-k-equal-sum-subsets/partition-to-k-equal-sum-subsets.cpp
@@ -4,17 +4,14 @@ public:
         int n=nums.size(),sum=accumulate(nums.begin(),nums.end(),0);
         if(sum%k) return 0;
         sum/=k;
-        vector<int> dp(1<<n,-1);
+        unordered_map<int,int> dp;
         dp[0]=0;
-        for(int i=0;i<(1<<n);i++){
-            // cout<<bitset<10>(i)<<" "<<dp[i]<<endl;
-            for(int j=0;j<n && dp[i]>=0;j++){
-                if(i&(1<<j) || dp[i]+nums[j]>sum) continue;
-                if((i|(1<<j))+1==1<<n) return 1;
-                dp[i|(1<<j)]=(dp[i]+nums[j])%sum;
+        for(int i=0;i<(1<<n) && !dp.count((1<<n)-1);i++){
+            for(int j=0;j<n && dp.count(i) && !dp.count((1<<n)-1);j++){
+                if((i&(1<<j))==0 && dp[i]+nums[j]<=sum) dp[i|(1<<j)]=(dp[i]+nums[j])%sum;
             }
         }
-        return 0;
+        return dp.count((1<<n)-1);
         
     }
 };
