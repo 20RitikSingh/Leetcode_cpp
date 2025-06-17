3e02f66cb2694a7dfe656aba205e18ed7159887d
Ritik Singh <92461786+20RitikSingh@users.noreply.github.com>
2025-06-17 11:55:28 +0530

    Time: 47 ms (59.39%) | Memory: 23.9 MB (21.50%) - LeetSync

--- 698-partition-to-k-equal-sum-subsets/partition-to-k-equal-sum-subsets.cpp
+++ 698-partition-to-k-equal-sum-subsets/partition-to-k-equal-sum-subsets.cpp
@@ -8,13 +8,13 @@ public:
         dp[0]=0;
         for(int i=0;i<(1<<n);i++){
             // cout<<bitset<10>(i)<<" "<<dp[i]<<endl;
-            if(dp[i]>=0)
-            for(int j=0;j<n;j++){
+            for(int j=0;j<n && dp[i]>=0;j++){
                 if(i&(1<<j) || dp[i]+nums[j]>sum) continue;
+                if((i|(1<<j))+1==1<<n) return 1;
                 dp[i|(1<<j)]=(dp[i]+nums[j])%sum;
             }
         }
-        return !dp[(1<<n)-1];
+        return 0;
         
     }
 };
