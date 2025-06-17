536f9f57b3cdb54220a6a843cf12fc8ae0f6467f
Ritik Singh <92461786+20RitikSingh@users.noreply.github.com>
2025-06-17 20:16:25 +0530

    Time: 8 ms (44.93%) | Memory: 8.9 MB (64.86%) - LeetSync

--- 3240-maximum-number-that-sum-of-the-prices-is-less-than-or-equal-to-k/maximum-number-that-sum-of-the-prices-is-less-than-or-equal-to-k.cpp
+++ 3240-maximum-number-that-sum-of-the-prices-is-less-than-or-equal-to-k/maximum-number-that-sum-of-the-prices-is-less-than-or-equal-to-k.cpp
@@ -4,7 +4,7 @@ public:
         long long low=1,high=1e15;
         while (low < high) {
             long long mid = low + (high - low + 1) / 2,res=0;
-            for(int i=x;i<56 && 2*mid>=(1LL<<i);i+=x) res+=(mid/(1LL<<i))*((1LL<<i)/2)+max(0LL,mid%(1LL<<i)-((1LL<<i)/2)+1);
+            for(int i=x;i<56;i+=x) res+=(mid/(1LL<<i))*((1LL<<i)/2)+max(0LL,mid%(1LL<<i)-((1LL<<i)/2)+1);
             if (res <= k) low=mid;
             else high = mid - 1;
         }
