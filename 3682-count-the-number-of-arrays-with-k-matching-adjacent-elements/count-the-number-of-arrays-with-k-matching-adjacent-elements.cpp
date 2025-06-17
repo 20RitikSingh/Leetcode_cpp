06533364a46d3821339e01d9e4bfe2c5a86674bf
Ritik Singh <92461786+20RitikSingh@users.noreply.github.com>
2025-06-18 02:23:08 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (75.79%) - LeetSync

--- 3682-count-the-number-of-arrays-with-k-matching-adjacent-elements/count-the-number-of-arrays-with-k-matching-adjacent-elements.cpp
+++ 3682-count-the-number-of-arrays-with-k-matching-adjacent-elements/count-the-number-of-arrays-with-k-matching-adjacent-elements.cpp
@@ -1,29 +1,28 @@
+const int MOD = 1e9 + 7;
+const int MAXN = 100000;
+long long fact[100001], invFact[100001];
+
+long long power(int x,int y){
+    if(y==0) return 1;
+    long long res=power(x,y/2);
+    res=res*res%MOD;
+    if(y%2) res=res*x%MOD;
+    return res;
+}
+struct Pre{
+    Pre(){
+        fact[0] = 1;
+        for (int i = 1; i <= MAXN; ++i)
+            fact[i] = fact[i - 1] * i % MOD;
+        invFact[MAXN] = power(fact[MAXN], MOD - 2);        
+        for (int i = MAXN; i > 0; --i)         
+            invFact[i - 1] = invFact[i] * i % MOD;
+    }
+};
+Pre init;
 class Solution {
-
-    const int MOD = 1e9 + 7;
-
-    long long fact[100001], invFact[100001];
-    long long power(long long a, long long b) {
-        long long res = 1;
-        while(b > 0) {
-            if(b & 1) res = res * a % MOD;
-            a = a * a % MOD;
-            b >>= 1;
-        }
-        return res;
-    }
-
-    void precompute(int n) {
-        fact[0] = invFact[0] = 1;
-        for(int i = 1; i <=n; ++i) {
-            fact[i] = fact[i-1] * i % MOD;
-            invFact[i] = power(fact[i], MOD - 2);
-        }
-    }
-
 public:
     int countGoodArrays(int n, int m, int k) {
-        precompute(max(m,n));
         return m*power(m-1,n-k-1)%MOD*fact[n-1]%MOD * invFact[k] % MOD * invFact[n -1-k] % MOD;;
     }
 };
