71e3a3782d78e6e9403738c84e08f7a6970e7c19
Ritik Singh <92461786+20RitikSingh@users.noreply.github.com>
2025-06-17 11:52:36 +0530

    Time: 57 ms (56.48%) | Memory: 24.1 MB (19.40%) - LeetSync

--- 698-partition-to-k-equal-sum-subsets/partition-to-k-equal-sum-subsets.cpp
+++ 698-partition-to-k-equal-sum-subsets/partition-to-k-equal-sum-subsets.cpp
@@ -1,33 +1,20 @@
 class Solution {
-    char dp[17][1<<16];
-    bool rec(int idx,int msk){
-        if(idx==v.size()) return 1;
-        if(dp[idx][msk]!=-1) return dp[idx][msk];
-        for(int i:v[idx]){
-            if((msk&i)==msk && rec(idx+1,msk|i)){
-                dp[idx][msk]=1;
-                return 1;
-            }
-        }
-        dp[idx][msk]=0;
-        return 0;
-    }
-    vector<vector<int>> v;
 public:
     bool canPartitionKSubsets(vector<int>& nums, int k) {
         int n=nums.size(),sum=accumulate(nums.begin(),nums.end(),0);
-        if(sum%k) return 0;sum/=k;
-        v.resize(k+1);
-        
-        for(int i=1;i<(1<<n);i++){
-            int x=0;
-            for(int j=0;j<n;j++) if(i&(1<<j)) x+=nums[j];
-            if(x%sum) continue;
-            v[x/sum].push_back(i);
+        if(sum%k) return 0;
+        sum/=k;
+        vector<int> dp(1<<n,-1);
+        dp[0]=0;
+        for(int i=0;i<(1<<n);i++){
+            // cout<<bitset<10>(i)<<" "<<dp[i]<<endl;
+            if(dp[i]>=0)
+            for(int j=0;j<n;j++){
+                if(i&(1<<j) || dp[i]+nums[j]>sum) continue;
+                dp[i|(1<<j)]=(dp[i]+nums[j])%sum;
+            }
         }
-
-        memset(dp,-1,sizeof(dp));
-        return rec(1,0);
+        return !dp[(1<<n)-1];
         
     }
 };
