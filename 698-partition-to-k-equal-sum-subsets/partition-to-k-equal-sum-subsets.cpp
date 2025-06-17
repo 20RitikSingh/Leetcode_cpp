ee2b21dc277f6382dac1c54b755952ccc41b8db1
Ritik Singh <92461786+20RitikSingh@users.noreply.github.com>
2025-06-17 12:10:57 +0530

    Time: 414 ms (34.79%) | Memory: 62.8 MB (5.67%) - LeetSync

--- 698-partition-to-k-equal-sum-subsets/partition-to-k-equal-sum-subsets.cpp
+++ 698-partition-to-k-equal-sum-subsets/partition-to-k-equal-sum-subsets.cpp
@@ -2,15 +2,11 @@ class Solution {
 public:
     bool canPartitionKSubsets(vector<int>& nums, int k) {
         int n=nums.size(),sum=accumulate(nums.begin(),nums.end(),0);
-        if(sum%k) return 0;
-        sum/=k;
-        unordered_map<int,int> dp;
-        dp[0]=0;
-        for(int i=0;i<(1<<n) && !dp.count((1<<n)-1);i++){
-            for(int j=0;j<n && dp.count(i) && !dp.count((1<<n)-1);j++){
-                if((i&(1<<j))==0 && dp[i]+nums[j]<=sum) dp[i|(1<<j)]=(dp[i]+nums[j])%sum;
-            }
-        }
+        if(sum%k) return 0;sum/=k;
+        unordered_map<int,int> dp;dp[0]=0;
+        for(int i=0;i<(1<<n) && !dp.count((1<<n)-1);i++) 
+        for(int j=0;j<n && dp.count(i) && !dp.count((1<<n)-1);j++) 
+        if((i&(1<<j))==0 && dp[i]+nums[j]<=sum) dp[i|(1<<j)]=(dp[i]+nums[j])%sum;
         return dp.count((1<<n)-1);
         
     }
