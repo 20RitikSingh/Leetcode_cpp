1a950c73e2442f72dbd3f32188f897aba81a745f
Ritik Singh <92461786+20RitikSingh@users.noreply.github.com>
2025-06-17 12:13:43 +0530

    Time: 53 ms (58.01%) | Memory: 23.8 MB (25.26%) - LeetSync

--- 698-partition-to-k-equal-sum-subsets/partition-to-k-equal-sum-subsets.cpp
+++ 698-partition-to-k-equal-sum-subsets/partition-to-k-equal-sum-subsets.cpp
@@ -3,11 +3,11 @@ public:
     bool canPartitionKSubsets(vector<int>& nums, int k) {
         int n=nums.size(),sum=accumulate(nums.begin(),nums.end(),0);
         if(sum%k) return 0;sum/=k;
-        unordered_map<int,int> dp;dp[0]=0;
-        for(int i=0;i<(1<<n) && !dp.count((1<<n)-1);i++) 
-        for(int j=0;j<n && dp.count(i) && !dp.count((1<<n)-1);j++) 
+        vector<int> dp(1<<n,-1);dp[0]=0;
+        for(int i=0;i<(1<<n) && dp[(1<<n)-1]<0;i++) 
+        for(int j=0;j<n && dp[i]>=0 && dp[(1<<n)-1]<0;j++) 
         if((i&(1<<j))==0 && dp[i]+nums[j]<=sum) dp[i|(1<<j)]=(dp[i]+nums[j])%sum;
-        return dp.count((1<<n)-1);
+        return !dp[(1<<n)-1];
         
     }
 };
