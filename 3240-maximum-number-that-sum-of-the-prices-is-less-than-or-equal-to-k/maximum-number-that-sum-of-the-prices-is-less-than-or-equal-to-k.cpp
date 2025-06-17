668fc8fb6025867311fadf442d156a81c7713261
Ritik Singh <92461786+20RitikSingh@users.noreply.github.com>
2025-06-17 20:16:13 +0530

    Time: 7 ms (46.74%) | Memory: 9 MB (37.68%) - LeetSync

--- 3240-maximum-number-that-sum-of-the-prices-is-less-than-or-equal-to-k/maximum-number-that-sum-of-the-prices-is-less-than-or-equal-to-k.cpp
+++ 3240-maximum-number-that-sum-of-the-prices-is-less-than-or-equal-to-k/maximum-number-that-sum-of-the-prices-is-less-than-or-equal-to-k.cpp
@@ -1,16 +1,13 @@
-unordered_map<long long,long long> dp[9];
 class Solution {
 public:
     long long findMaximumNumber(long long k, int x) {
         long long low=1,high=1e15;
-        if(dp[x].count(k)) return dp[x][k];
         while (low < high) {
             long long mid = low + (high - low + 1) / 2,res=0;
-            for(int i=x;i<60;i+=x) res+=(mid/(1LL<<i))*((1LL<<i)/2)+max(0LL,mid%(1LL<<i)-((1LL<<i)/2)+1);
+            for(int i=x;i<56 && 2*mid>=(1LL<<i);i+=x) res+=(mid/(1LL<<i))*((1LL<<i)/2)+max(0LL,mid%(1LL<<i)-((1LL<<i)/2)+1);
             if (res <= k) low=mid;
             else high = mid - 1;
         }
-        dp[x][k]=low;
         return low;
     }
 };
