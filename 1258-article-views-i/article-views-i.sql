16325dea86c3e2cd46d62c6c2f83491bfafc3210
Ritik Singh <92461786+20RitikSingh@users.noreply.github.com>
2025-06-19 15:15:05 +0530

    Time: 1001 ms (17.29%) | Memory: 0B (100.00%) - LeetSync

--- 1258-article-views-i/article-views-i.sql
+++ 1258-article-views-i/article-views-i.sql
@@ -1,5 +1,2 @@
-# Write your MySQL query statement below
-select distinct viewer_id as id
-from Views 
-where viewer_id = author_id
-order by id;
+/* Write your T-SQL query statement below */
+select distinct v.author_id as id from Views v where v.author_id = v.viewer_id order by id
