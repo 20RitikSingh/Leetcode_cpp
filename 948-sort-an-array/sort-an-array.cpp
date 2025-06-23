9aece5391d4f1e629884f4986cfc2a97ad59051e
Ritik Singh <92461786+20RitikSingh@users.noreply.github.com>
2025-06-23 17:37:42 +0530

    Time: 183 ms (59.27%) | Memory: 128.4 MB (45.95%) - LeetSync

--- 948-sort-an-array/sort-an-array.cpp
+++ 948-sort-an-array/sort-an-array.cpp
@@ -1,7 +1,7 @@
-#define mid low+(high-low)/2
 class Solution {
     void mergesort(vector<int> &nums,int low,int high){
         if(high-low==1) return;
+        int mid=low+(high-low)/2;
         mergesort(nums,low,mid);
         mergesort(nums,mid,high);
         inplace_merge(nums.begin()+low,nums.begin()+mid,nums.begin()+high);
