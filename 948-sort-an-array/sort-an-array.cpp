edc1a9113b11f2f922b2e8ea64dc24575ae1ea32
Ritik Singh <92461786+20RitikSingh@users.noreply.github.com>
2025-06-23 17:36:01 +0530

    Time: 183 ms (59.27%) | Memory: 128.6 MB (45.95%) - LeetSync

--- 948-sort-an-array/sort-an-array.cpp
+++ 948-sort-an-array/sort-an-array.cpp
@@ -1,11 +1,14 @@
 class Solution {
+    void mergesort(vector<int> &nums,int low,int high){
+        if(high-low==1) return;
+        int mid=low+(high-low)/2;
+        mergesort(nums,low,mid);
+        mergesort(nums,mid,high);
+        inplace_merge(nums.begin()+low,nums.begin()+mid,nums.begin()+high);
+    }
 public:
     vector<int> sortArray(vector<int>& nums) {
-        multiset<int> set(nums.begin(),nums.end());
-        for(int &i:nums){ 
-            i=*set.begin();
-            set.erase(set.begin());
-        }
+        mergesort(nums,0,nums.size());
         return nums;
     }
 };
