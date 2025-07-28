c5c6359b4b053aea21c66c9ae48f2464b922c110
Ritik Singh <92461786+20RitikSingh@users.noreply.github.com>
2025-07-29 01:28:54 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (56.44%) - LeetSync

--- 233-number-of-digit-one/number-of-digit-one.cpp
+++ 233-number-of-digit-one/number-of-digit-one.cpp
@@ -4,18 +4,16 @@ class Solution {
         if(dp[idx][tight][count]!=-1) return dp[idx][tight][count];
         if(idx==v.size()) return tight?0:count;
         int res=0;
-        for(int i=0;i<10;i++){
-            res+=rec(v,idx+1,v[idx]<i?1:(v[idx]==i?tight:0),count+(i==1));
-        }
+        for(int i=0;i<v[idx];i++) res+=rec(v,idx+1,v[idx]==i?tight:0,count+(i==1));
+        res+=rec(v,idx+1,tight,count+(v[idx]==1));
+        for(int i=v[idx]+1;i<10;i++) res+=rec(v,idx+1,1,count+(i==1));
         return dp[idx][tight][count]=res;
     }
 public:
     int countDigitOne(int n) {
         if(!n) return 0;
         vector<int> v;
-        while(n){
-            v.push_back(n%10),n/=10;
-        }
+        while(n) v.push_back(n%10),n/=10;
         memset(dp,-1,sizeof(dp));
         return rec(v,0,0,0);
     }
